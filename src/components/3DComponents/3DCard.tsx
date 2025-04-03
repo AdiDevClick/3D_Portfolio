d7cd9191369ce608f09cb7c3fb4103c476a56067
Adrien <adrienquijo@gmail.com>
2025-04-03 19:39:21 +0200

    Les cards seront maintenant dans un <group/> - Suppression de l'animation pour intégrer le descriptif dans le Canvas -

--- src/components/3DComponents/3DCard.tsx
+++ src/components/3DComponents/3DCard.tsx
@@ -1,19 +1,23 @@
-import { Image, RoundedBox, Text } from '@react-three/drei';
+import { Html, Image, Scroll, Text, useScroll } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { easing } from 'maath';
-import { useEffect, useMemo, useRef } from 'react';
-import { DoubleSide, Mesh, SkinnedMesh } from 'three';
+import { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
+import { DoubleSide, Group, Mesh, Vector3 } from 'three';
 import { throttle } from '../../functions/promises.js';
 import {
     ElementType,
     ReducerType,
 } from '../../hooks/reducers/carouselTypes.js';
-import { deltaTime } from 'three/src/nodes/TSL.js';
+import { getSidesPositions } from '../../functions/3Dmodels.js';
+import { useNavigate } from 'react-router';
+import '@css/Card.scss';
 
 interface AdditionalProps {
     visibleWireframe?: boolean;
     presenceRadius?: number;
     presenceCircle?: boolean;
+    BENDING: number;
+    y_HEIGHT: number;
 }
 
 type CardProps = {
@@ -21,34 +25,60 @@ type CardProps = {
     card: ElementType;
 } & AdditionalProps;
 
+/**
+ * Composant Carte
+ */
 export default function Card({ reducer, card, ...props }: CardProps) {
-    const { id, ...rest } = card;
-    const cardRef = useRef<Mesh>(null);
+    const groupRef = useRef<Group>(null!);
+    const cardRef = useRef<Mesh>(null!);
+    const htmlRef = useRef<HTMLElement>(null!);
+    const htmlGroupRef = useRef<HTMLElement>(null!);
+    const spherePresenceRef = useRef<Mesh>(null!);
+
+    // État local pour savoir si la carte est cliquée
+    // const [isClicked, setIsClicked] = useState(false);
+    const [bending, setBending] = useState(props.BENDING);
+    const [width, setWidth] = useState(card.baseScale);
+    const [htmlWidth, setHtmlWidth] = useState(0);
+    const scroll = useScroll();
+
+    // Références pour l'animation (pour éviter de re-render)
+    // Facteur d'animation (t de 0 à 1)
+    const animationProgressRef = useRef(0);
+    const startPositionRef = useRef<Vector3>(null!);
+
     const cardHoverScale = card.isActive ? 1.15 : 1;
     const cardHoverRadius = card.isActive ? 0.25 : 0.1;
     const cardHoverZoom = card.isActive ? 1 : 1.5;
 
-    if (card.presenceRadius && card.presenceRadius !== 0.5)
-        console.log('different radius : ', card.presenceRadius);
+    const navigate = useNavigate();
 
     const onClickHandler = (e) => {
         e.stopPropagation();
+        // Deny any other clicked elements if one is opened
+        if (reducer.activeContent?.id === card.id) reducer.clickElement(card);
+        if (!reducer.activeContent) reducer.clickElement(card);
+        if (cardRef.current) {
+            startPositionRef.current = cardRef.current.position.clone();
+            // Réinitialisez la progression d'animation
+            animationProgressRef.current = 0;
+        }
         // reducer.animate(card, 'dropoff');
+        // navigate('/error/page');
+        // return <Navigate to={'/error/page'} replace={true} />;
     };
     const onHover = (e) => {
         e.stopPropagation();
-        reducer.activateElement(card);
+        if (reducer.activeContent?.isClicked) return;
+        reducer.activateElement(card, true);
     };
 
     const onPointerOut = (e) => {
         e.stopPropagation();
-        if (card.isActive) {
-            reducer.activateElement(card);
-            console.log(card.presenceRadius);
-        }
+        reducer.activateElement(card, false);
     };
 
-    const throttledUpdate = useMemo(
+    const throttledUpdatedScale = useMemo(
         () =>
             throttle((newScale: number) => {
                 reducer.updateScale(card, newScale);
@@ -56,50 +86,67 @@ export default function Card({ reducer, card, ...props }: CardProps) {
         [reducer, card]
     );
 
+    // const throttledUpdatedPosition = useMemo(() => {
+    //     getSidesPositions(card, cardRef);
+    //     throttle((newScale: number) => {
+    //         reducer.updatePosition(card, newScale);
+    //     }, 100);
+    // }, [reducer, card]);
+
     useFrame((state, delta) => {
         if (!cardRef.current) return;
+        const { material, scale, position } = cardRef.current;
 
-        // Scale the card size up
-        easing.damp3(cardRef.current.scale, cardHoverScale, 0.15, delta);
+        if (!card.isClicked) {
+            // Scale the card size up
+            easing.damp3(scale, cardHoverScale, 0.15, delta);
+            // Modify card radius to be rounder
+            easing.damp(material, 'radius', cardHoverRadius, 0.2, delta);
+            // Scale the zoom inside the plane mesh
+            easing.damp(material, 'zoom', cardHoverZoom, 0.2, delta);
 
-        // Modify card radius to be rounder
-        easing.damp(
-            cardRef.current.material,
-            'radius',
-            cardHoverRadius,
-            0.2,
-            delta
-        );
+            const newScale = scale.x;
 
-        // Scale the zoom inside the plane mesh
-        easing.damp(
-            cardRef.current.material,
-            'zoom',
-            cardHoverZoom,
-            0.2,
-            delta
-        );
+            // Bending effect on Hover / non hover
+            if (card.isActive) {
+                if (bending > 0) setBending((prev) => prev - delta);
+                if (width < card.baseScale + 0.2)
+                    setWidth((prev) => prev + delta);
+            } else {
+                setBending((prev) => Math.min(prev + delta, props.BENDING));
+                if (width > card.baseScale) {
+                    setWidth((prev) => prev - delta);
+                } else {
+                    setWidth((prev) => Math.min(prev + delta, card.baseScale));
+                }
+            }
 
-        if (card.animation === 'dropoff') {
-            card.position.x =
-                Math.sin(card.position.x * 0.1 + 0.25) -
-                Math.sqrt(5 ^ (2 - card.position.x) ^ 3);
-        }
-        const newScale = cardRef.current.scale.x;
-
-        card.isActive
-            ? easing.damp3(cardRef.current.position, [cardRef.current.position.x, cardRef.current.position.y, cardRef.current.position.z - 1], 0.15, delta)
-            : // ? easing.damp3(cardRef.current.position, [0.5, 0, -2], 0.15, delta)
-              easing.damp3(
-                  cardRef.current.position,
-                  card.position,
-                  0.15,
-                  delta
-              );
-
-        // Save new scale property for the card
-        if (newScale !== card.currentScale) {
-            throttledUpdate(newScale);
+            if (newScale !== card.currentScale) {
+                throttledUpdatedScale(newScale);
+            }
+            // Et forcer la rotation X à revenir à 0 (ou autre valeur par défaut) en mode hover
+            easing.damp(cardRef.current.rotation, 'x', 0, 0.15, delta);
+        } else {
+            // state.camera.position.lerp(cardRef.current.position, 0.32);
+            cardRef.current.scale.lerp(new Vector3(1.5, 1.5, 1.5), 0.1);
+            if (bending > 0) setBending((prev) => prev - delta);
+            if (width < card.baseScale + 0.2) setWidth((prev) => prev + delta);
+            // console.log(-scroll.offset * (Math.PI * 2));
+            // htmlGroupRef.current.rotation.y = -scroll.offset * (Math.PI * 2); // Rotate contents
+            if (card.animation === 'dropoff') {
+                easing.damp3(
+                    position,
+                    Math.sin(card.position.x * 0.1 + 0.25) -
+                        Math.sqrt(5 ^ (2 - card.position.x) ^ 3),
+                    0.15,
+                    delta
+                );
+                // card.position.x =
+                //     Math.sin(card.position.x * 0.1 + 0.25) -
+                //     Math.sqrt(5 ^ (2 - card.position.x) ^ 3);
+            }
+            // easing.damp3(state.camera.position, [0, -10, 0], 0.3, delta);
+            // state.camera.position.set(htmlGroupRef.current.position);
         }
     });
 
@@ -108,37 +155,209 @@ export default function Card({ reducer, card, ...props }: CardProps) {
      */
     useEffect(() => {
         if (cardRef.current) {
+            // Force calculated default position
+            cardRef.current.position.set(
+                card.position.x,
+                card.position.y,
+                card.position.z
+            );
+
+            // Save presence Radius in sphere
+            // spherePresenceRef.current.presenceRadius = 0.5 * card.baseScale;
+
             reducer.updateElements({
                 ...card,
                 ref: cardRef,
                 presenceRadius: props.presenceRadius,
+                spacePositions: getSidesPositions(card, cardRef),
             });
+
+            // Supposons que la hauteur de la carte se trouve dans la géométrie (par exemple, boxGeometry)
+            // const geomParams = cardRef.current.geometry.parameters;
+            // if (geomParams && geomParams.height) {
+            //     // On déplace le mesh vers le bas de façon que le haut du mesh
+            //     // soit aligné avec l'origin du groupe
+            //     cardRef.current.position.y = -geomParams.height / 2;
+            // }
         }
     }, []);
 
+    useLayoutEffect(() => {
+        if (htmlRef.current) {
+            setHtmlWidth(htmlRef.current.getBoundingClientRect().width);
+            console.log(htmlRef.current.getBoundingClientRect().width);
+        }
+    }, [htmlRef, htmlWidth, card]);
+
     return (
-        <Image
-            ref={cardRef}
-            url={card.url}
-            transparent
-            side={DoubleSide}
+        <group
+            ref={groupRef}
             onPointerOver={onHover}
             onPointerOut={onPointerOut}
             onClick={onClickHandler}
-            {...rest}
         >
-            {props.visibleWireframe && (
-                <meshBasicMaterial
-                    visible={props.visibleWireframe}
-                    wireframe
-                    side={DoubleSide}
+            <Image
+                ref={cardRef}
+                url={card.url}
+                transparent
+                side={DoubleSide}
+                rotation={card.rotation}
+                // position={card.position}
+                scale={[width - 0.1, width]}
+                // sidePositions={card.sidePositions}
+            >
+                {props.visibleWireframe && (
+                    <meshBasicMaterial
+                        visible={props.visibleWireframe}
+                        wireframe
+                        side={DoubleSide}
+                    />
+                )}
+
+                <bentPlaneGeometry
+                    args={[bending, width, props.y_HEIGHT, 20]}
                 />
-            )}
-            <bentPlaneGeometry args={[0.2, card.baseScale, 2, 20]} />
-            <mesh visible={props.presenceCircle}>
-                <sphereGeometry args={[props.presenceRadius, 32]} />
-                <meshBasicMaterial color={'red'} wireframe />
-            </mesh>
-        </Image>
+
+                <mesh ref={spherePresenceRef} visible={props.presenceCircle}>
+                    <sphereGeometry args={[props.presenceRadius, 32]} />
+                    <meshBasicMaterial color={'red'} wireframe />
+                </mesh>
+                {card.isClicked && (
+                    // <Scroll>
+                    // <Rig>
+                    <group ref={htmlGroupRef}>
+                        <Html
+                            // fullscreen
+                            // portal={document.body}
+                            ref={htmlRef}
+                            position={
+                                reducer.isMobile
+                                    ? [0, -1.5, 0]
+                                    : [-width - 0.1, 0, 0]
+                            }
+                            transform
+                            distanceFactor={1}
+                            rotation={[0, 3.2, 0]}
+                            anchorX={0}
+                            anchorY={0}
+                        >
+                            <div
+                                className="opened-content"
+                                onClick={onClickHandler}
+                                // style={{ top: '100vh' }}
+                            >
+                                {/* <img src={card.url} alt="Preview Project" /> */}
+                                <h2>{card.title}</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Cupiditate doloribus ex
+                                    quam repellendus assumenda eligendi nam
+                                    rerum aut sequi nulla itaque atque, et
+                                    neque, ea minima magni fugit sunt quibusdam!
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Magni commodi fuga sed
+                                    asperiores neque amet corporis in facilis,
+                                    aliquam hic? Esse ea ipsa magni modi! Unde
+                                    totam quo quos ullam. Lorem, ipsum dolor sit
+                                    amet consectetur adipisicing elit. Maiores
+                                    delectus et illum placeat ea, dolore
+                                    accusamus animi, labore saepe quas
+                                    architecto corrupti, quo nulla ullam
+                                    nesciunt cum nisi voluptatem tempore. Lorem
+                                    ipsum dolor sit amet consectetur adipisicing
+                                    elit. Placeat nisi at quo natus odit
+                                    consectetur eius exercitationem, assumenda
+                                    ducimus sunt dolores voluptatem molestias,
+                                    cum nulla suscipit magni possimus! Harum,
+                                    libero.
+                                </p>
+                                {/* <p>{card.description}</p> */}
+                                <div className="tech-stack">
+                                    {/* Affichage des logos, etc. */}
+                                </div>
+                                <button>Retour</button>
+                            </div>
+                        </Html>
+                    </group>
+
+                    //</Rig>
+                    // </Scroll>
+                )}
+            </Image>
+        </group>
     );
+}
+function Rig(props) {
+    const ref = useRef(null);
+    const scroll = useScroll();
+    useFrame((state, delta) => {
+        // console.log(scroll);
+        // ref.current.lookAt(1, 0.5, 0); // Look at center
+        // ref.current.position.y = 1; // Rotate contents
+        // ref.current.rotation.y = -scroll.offset * 2; // Rotate contents
+        // ref.current.rotation.y = -scroll.offset * (Math.PI * 2); // Rotate contents
+        // state.events.update(); // Raycasts every frame rather than on pointer-move
+        // Move camera
+        // easing.damp3(
+        //     state.camera.position,
+        //     [-state.pointer.x * 2, state.pointer.y + 1.5, 10],
+        //     0.3,
+        //     delta
+        // );
+        // state.camera.lookAt(0, 0, 0); // Look at center
+    });
+    return <group ref={ref} {...props} />;
+}
+
+<group>
+    {/* <Text
+    key={'key-text' + card.id}
+    maxWidth={0.1}
+    anchorY={0}
+    fontSize={0.1}
+    // rotation={[0, card.cardAngles?.active, 0]}
+    rotation={[0, 3.15, 0]}
+    anchorX={0}
+    color={'red'}
+    position={[-0.5, 1, -0.05]}
+    visible={card.isActive}
+>
+    test
+</Text> */}
+
+    {/* // <Text
+//     maxWidth={0.1}
+//     position={[0, 0, -0.1]}
+//     // position={[
+//     //     cardRef.current?.position.x,
+//     //     cardRef.current?.position.y,
+//     //     cardRef.current?.position.z,
+//     // ]}
+//     anchorY={'top'}
+//     anchorX={'center'}
+//     fontSize={0.1}
+//     rotation={[0, 3.15, 0]}
+//     color={'white'}
+// >
+//     test2
+// </Text> */}
+</group>;
+{
+    /* {card.isClicked && (
+                <Text
+                    maxWidth={0.1}
+                    position={[
+                        cardRef.current?.position.x,
+                        cardRef.current?.position.y,
+                        cardRef.current?.position.z - 0.1,
+                    ]}
+                    anchorY={'top'}
+                    anchorX={-1}
+                    fontSize={0.1}
+                    rotation={[0, card.cardAngles?.active, 0]}
+                    color={'white'}
+                >
+                    test2
+                </Text>
+            )} */
 }
