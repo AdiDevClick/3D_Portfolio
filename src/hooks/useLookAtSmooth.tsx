c845b6a5de218c2988a7f1775760ef930dcf077c
Adrien <adrienquijo@gmail.com>
2025-04-03 19:45:50 +0200

    Création d'un hook permettant de faire naviguer la caméra sur une position en utilisant la quaternion cam - Hook utilisable seulement dans un Canva

---
+++ src/hooks/useLookAtSmooth.tsx
@@ -0,0 +1,50 @@
+import { useFrame, useThree } from '@react-three/fiber';
+import { useRef } from 'react';
+import { Vector3 } from 'three';
+
+/**
+ * @param speed - between 0 to 1, 1 being instant
+ */
+export function useLookAtSmooth(speed = 0.05) {
+    const { camera } = useThree();
+    const lookAtProgress = useRef(0); // 0 to 1
+    const lookAtTarget = useRef(null);
+
+    /**
+     * Ensure position passed is of type THREE.Vector3
+     */
+    const lookAtSmooth = (position: Vector3) => {
+        lookAtProgress.current = 0;
+        lookAtTarget.current = position;
+    };
+
+    useFrame(() => {
+        if (lookAtTarget?.current !== null) {
+            // clone camera to get the target quaternion using lookAt()
+            const clone = camera.clone();
+            clone.lookAt(lookAtTarget.current);
+            if (
+                Math.abs(clone.quaternion._y - camera.quaternion._y) < 0.01 &&
+                Math.abs(clone.quaternion._w - camera.quaternion._w) < 0.01 &&
+                Math.abs(clone.quaternion._x - camera.quaternion._x) < 0.01 &&
+                Math.abs(clone.quaternion._z - camera.quaternion._z) < 0.01
+            ) {
+                // the differernce between the clone's quaternion and the camera's
+                // are close enough, so we clear the target to complete the animation
+                lookAtTarget.current = null;
+            } else {
+                // take a step towards target quaternion
+                lookAtProgress.current = Math.min(
+                    lookAtProgress.current + speed,
+                    1
+                );
+                camera.quaternion.slerp(
+                    clone.quaternion,
+                    lookAtProgress.current
+                );
+            }
+        }
+    });
+
+    return { lookAtSmooth };
+}
