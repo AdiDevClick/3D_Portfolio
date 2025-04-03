beb0775c54798a8d8840fad23a38448421019f72
Adrien <adrienquijo@gmail.com>
2025-04-03 19:47:26 +0200

    Modification du Type des elements du Carousel pour être en accord avec les dernières mise à jour

--- src/hooks/reducers/carouselTypes.ts
+++ src/hooks/reducers/carouselTypes.ts
@@ -1,8 +1,20 @@
-import { PlaneGeometry, Vector3 } from 'three';
+import { Ref, RefObject } from 'react';
+import { Mesh, Vector3 } from 'three';
 
 export interface CarouselState {
     elements: ElementType[]; // Tableau des éléments
 }
+
+type sidesPositions = {
+    bottom: number;
+    top: number;
+    left: number;
+    right: number;
+    front: number | null;
+    back: number | null;
+};
+
+type typeAngles = { active: number; onHold: number };
 
 export interface ElementType {
     id: string;
@@ -10,20 +22,28 @@ export interface ElementType {
     baseScale: number;
     url: string;
     position: Vector3;
+    rotation: [number, number, number];
     visibleWireframe?: boolean;
     presenceRadius?: number;
     presenceCircle?: boolean;
     isActive?: boolean;
-    ref?: PlaneGeometry;
+    isClicked?: boolean;
+    ref?: RefObject<Mesh>;
     animation?: string;
+    containerScale?: number;
+    spacePositions?: sidesPositions;
+    cardAngles?: typeAngles;
 }
 
 export interface ReducerType {
     addElements: (element: ElementType) => void;
-    activateElement: (element: ElementType) => void;
+    activateElement: (element: ElementType, property: boolean) => void;
+    clickElement: (element: ElementType) => void;
     animate: (element: ElementType, animationName: string) => void;
     updateScale: (element: ElementType, number: number) => void;
     updateElements: (element: ElementType) => void;
     deleteElements: (element: ElementType) => void;
     showElements: ElementType[];
+    activeContent: ElementType | null | undefined;
+    contentSizes: number[] | null | undefined;
 }
