26a8026b5e51f6fee8c50fe9fa59f4da6204cfd9
Adrien <adrienquijo@gmail.com>
2025-04-05 15:11:07 +0200

    Ajout de la doc et du typage du helper

--- src/components/3DComponents/Helper.tsx
+++ src/components/3DComponents/Helper.tsx
@@ -1,6 +1,20 @@
-import { AxesHelper } from 'three';
+import { RefObject } from 'react';
+import { AxesHelper, Mesh } from 'three';
 
-export function Helper(position = true, rotation = false, ref, size = 2) {
+/**
+ * Ajoute un "Helper" à l'élément 3D qui montre
+ * la direction ainsi que la position de l'élément dans l'espace
+ * @param [size=2] - La taille du helper
+ * @param [position=true] - Montre la position
+ * @param [rotation=false]  - Montre la rotation
+ * @param ref - La référence de l'élément où l'on attachera le helper
+ */
+export function Helper(
+    position = true,
+    rotation = false,
+    ref: RefObject<Mesh>,
+    size = 2
+) {
     const helper = new AxesHelper(size);
     if (position) helper.position.copy(ref.current.position);
     if (rotation) helper.rotation.copy(ref.current.rotation);
