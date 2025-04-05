a4b10de0a3b05f9c6b5f694d4b7195ad12c39271
Adrien <adrienquijo@gmail.com>
2025-04-05 16:54:08 +0200

    Ajout de JSDoc

--- src/hooks/reducers/carouselTypes.ts
+++ src/hooks/reducers/carouselTypes.ts
@@ -33,18 +33,30 @@ export interface ElementType {
     containerScale?: number;
     spacePositions?: sidesPositions;
     cardAngles?: typeAngles;
+    stack: {};
 }
 
 export interface ReducerType {
+    /** Add an element to the State */
     addElements: (element: ElementType) => void;
+    /** Activate or Deactivate the hover State */
     activateElement: (element: ElementType, property: boolean) => void;
+    /** Activate or Deactivate the clicked State */
     clickElement: (element: ElementType) => void;
+    /** Store animations */
     animate: (element: ElementType, animationName: string) => void;
+    /** Update the card Scale */
     updateScale: (element: ElementType, number: number) => void;
+    /** Add some new properties to the element - Avoid using it */
     updateElements: (element: ElementType) => void;
+    /** Delete an element based on it's ID */
     deleteElements: (element: ElementType) => void;
+    /** Display all the elements in the State */
     showElements: ElementType[];
+    /** Store the current hovered/clicked element */
     activeContent: ElementType | null | undefined;
+    /** Store the viewport width and height */
     contentSizes: number[] | null | undefined;
+    /** Store the boolean value of a mobile device or not mobile */
     isMobile: boolean;
 }
