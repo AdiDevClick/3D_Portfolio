d33f56ac60eb86e6e604288df4a13ec46da4abe5
Adrien <adrienquijo@gmail.com>
2025-04-05 16:24:29 +0200

    Ajout de le JSDoc

--- src/components/tags/TagsContainer.tsx
+++ src/components/tags/TagsContainer.tsx
@@ -1,17 +1,22 @@
 import { Tags } from '@/components/tags/Tags.tsx';
+import { ElementType } from '@/hooks/reducers/carouselTypes.ts';
 import { useId } from 'react';
+type DatasType = {
+    /** La propriété "stack" de la card contenant toute la stack technique */
+    datas: ElementType['stack'];
+};
 
 /**
  * Crer des tags puis les affiches dans un container
  */
-export function TagsContainer({ datas }) {
+export function TagsContainer({ datas }: DatasType) {
     Array.isArray(datas) ? datas : [datas];
     return (
         <ul className="tags_container">
             {Object.entries(datas).map(([text, logoUrl]) => {
                 const id = useId();
                 return (
-                    <Tags logo={logoUrl} key={id}>
+                    <Tags key={id} logo={logoUrl}>
                         {text}
                     </Tags>
                 );
