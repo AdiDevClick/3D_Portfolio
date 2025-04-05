bdb0ae64a4c2d1bd59155829389ee29289ea2f77
Adrien <adrienquijo@gmail.com>
2025-04-05 16:24:54 +0200

    Ajout JSDoc

--- src/components/tags/Tags.tsx
+++ src/components/tags/Tags.tsx
@@ -1,10 +1,13 @@
 import { ReactNode } from 'react';
 type TagsTypes = {
     children: ReactNode;
+    /** url du logo */
     logo: string;
 };
+
 /**
  * Affiche un Tag contenant un texte ainsi qu'un logo
+ * @param logo - Url du logo
  */
 export function Tags({ children, logo }: TagsTypes) {
     return (
