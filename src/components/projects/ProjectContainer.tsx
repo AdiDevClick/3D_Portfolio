47810a6d8077584676a6aa5e2bcc06b6521aa66f
Adrien <adrienquijo@gmail.com>
2025-04-05 17:15:37 +0200

    Restauration du scroll wheel sur le container -

--- src/components/projects/ProjectContainer.tsx
+++ src/components/projects/ProjectContainer.tsx
@@ -1,5 +1,6 @@
 import { ProjectContent } from '@/components/projects/ProjectContent.tsx';
 import { ElementType } from '@/hooks/reducers/carouselTypes.ts';
+import { WheelEvent } from 'react';
 
 type ProjectContainerTypes = {
     onClick: () => void;
@@ -11,8 +12,17 @@ export function ProjectContainer({
     card,
     ...props
 }: ProjectContainerTypes) {
+    const onScrollHandler = (e: WheelEvent) => {
+        e.stopPropagation();
+    };
+
     return (
-        <div className="opened-content" onClick={onClick} {...props}>
+        <div
+            className="opened-content"
+            onClick={onClick}
+            onWheel={onScrollHandler}
+            {...props}
+        >
             <ProjectContent card={card} {...props} />
         </div>
     );
