7cead26ac0de5ec48f4f0a07555f07eea97ebfe1
Adrien <adrienquijo@gmail.com>
2025-04-05 15:05:28 +0200

    Création d'un composant qui contiendra la liste des tags

--- src/components/tags/TagsContainer.tsx
+++ src/components/tags/TagsContainer.tsx
@@ -1,16 +1,21 @@
 import { Tags } from '@/components/tags/Tags.tsx';
+import { useId } from 'react';
 
 /**
  * Crer des tags puis les affiches dans un container
  */
 export function TagsContainer({ datas }) {
     Array.isArray(datas) ? datas : [datas];
-
     return (
         <ul className="tags_container">
-            {datas.map((text, index) => (
-                <Tags key={index}>{text}</Tags>
-            ))}
+            {Object.entries(datas).map(([text, logoUrl]) => {
+                const id = useId();
+                return (
+                    <Tags logo={logoUrl} key={id}>
+                        {text}
+                    </Tags>
+                );
+            })}
         </ul>
     );
 }
