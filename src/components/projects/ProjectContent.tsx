9cbc1b3523b73ba05e7dc38c9edaa1b5b07b176a
Adrien <adrienquijo@gmail.com>
2025-04-09 12:47:40 +0200

    Ajout d'une icône "étoile" à côté du contenu pour coller à la maquette - Modification du layout

--- src/components/projects/ProjectContent.tsx
+++ src/components/projects/ProjectContent.tsx
@@ -1,5 +1,6 @@
 import { TagsContainer } from '@/components/tags/TagsContainer.tsx';
 import { ElementType } from '@/hooks/reducers/carouselTypes.ts';
+import starIcon from '@icons/star.svg';
 
 type cardType = {
     card: ElementType;
@@ -11,9 +12,10 @@ type cardType = {
 export function ProjectContent({ card }: cardType) {
     return (
         <>
+            <span className="card__close"></span>
             {/* <img src={card.url} alt="Preview Project" /> */}
             <h2 className="card__title">{card.title}</h2>
-            <p>
+            {/* <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Cupiditate doloribus ex quam repellendus assumenda eligendi nam
                 rerum aut sequi nulla itaque atque, et neque, ea minima magni
@@ -28,10 +30,24 @@ export function ProjectContent({ card }: cardType) {
                 natus odit consectetur eius exercitationem, assumenda ducimus
                 sunt dolores voluptatem molestias, cum nulla suscipit magni
                 possimus! Harum, libero.
-            </p>
-            {/* <p>{card.description}</p> */}
+            </p> */}
+            <p className="card__description">{card.description}</p>
+            <ul className="card__content">
+                {card.content.map((text, index) => (
+                    <li className="content__item" key={index}>
+                        <svg
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M12 1C12 1 12 8 10 10C8 12 1 12 1 12C1 12 8 12 10 14C12 16 12 23 12 23C12 23 12 16 14 14C16 12 23 12 23 12C23 12 16 12 14 10C12 8 12 1 12 1Z"></path>
+                        </svg>
+                        <p>{text}</p>
+                    </li>
+                ))}
+            </ul>
             <TagsContainer datas={card.stack} />
-            <button>Retour</button>
         </>
     );
 }
