f6884f3929e0f5aca829fcfd1307949b3618f41c
Adrien <adrienquijo@gmail.com>
2025-04-09 12:44:59 +0200

    Le conteneur HTML est maintenant responsive - Il calcule la width de la fenêtre par rapport au viewport et renvoi le chiffre sous forme de propriété style pour que le CSS ajuste automatiquement les dimensions en fonction du device -

--- src/components/3DComponents/Html/HtmlContainer.tsx
+++ src/components/3DComponents/Html/HtmlContainer.tsx
@@ -1,6 +1,8 @@
+import { debounce } from '@/functions/promises.js';
 import { ReducerType } from '@/hooks/reducers/carouselTypes.ts';
 import { Html } from '@react-three/drei';
-import { ReactNode } from 'react';
+import { useFrame } from '@react-three/fiber';
+import React, { ReactNode, useMemo, useRef, useState } from 'react';
 
 type HtmlContainerTypes = {
     width: number;
@@ -17,19 +19,62 @@ export function HtmlContainer({
     reducer,
     ...props
 }: HtmlContainerTypes) {
+    const htmlRef = useRef<HTMLElement>(null);
+    // useMutationObserver(handleObserver, htmlRef);
+    const [scaleRatio, setScaleRatio] = useState(1);
+    const [done, setDone] = useState(false);
+
+    const debouncedUpdateScale = useMemo(
+        () =>
+            debounce((newScale: number) => {
+                setScaleRatio(newScale);
+            }, 15),
+        []
+    );
+
+    const measure = () => {
+        if (!htmlRef.current) {
+            return;
+        }
+        const rect = htmlRef.current.getBoundingClientRect();
+        const viewportWidth = htmlRef.current.clientWidth;
+        const newRatio = viewportWidth / rect.width;
+
+        if (rect.width !== viewportWidth && newRatio !== scaleRatio && !done) {
+            setScaleRatio(newRatio);
+        } else {
+            setDone(true);
+        }
+    };
+
+    useFrame(() => {
+        if (done) return;
+        if (htmlRef.current) {
+            measure();
+        }
+    });
+
     return (
         <Html
             // fullscreen
-            // portal={document.body}
-            // ref={htmlRef}
-
+            // portal={document.body.main}
+            // sprite
+            ref={htmlRef}
             transform
             distanceFactor={1}
+            style={{ '--data-custom-scale': scaleRatio }}
             // rotatio n={[0, 3.2, 0]}
             // anchorX={1}
             // anchorY={1}
             {...props}
         >
+            {/* {React.Children.map(children, (child) =>
+                React.isValidElement(child)
+                    ? React.cloneElement(child, {
+                          'data-custom-scale': scaleRatio,
+                      })
+                    : child
+            )} */}
             {children}
         </Html>
     );
@@ -52,3 +97,53 @@ export function HtmlContainer({
 
 // // Si nécessaire, forcer le rafraîchissement des matrices
 // camera.updateMatrixWorld();
+function mergeRefs<T>(...refs: (React.Ref<T> | undefined)[]) {
+    return (node: T) => {
+        refs.forEach((ref) => {
+            if (typeof ref === 'function') {
+                ref(node);
+            } else if (ref != null) {
+                ref.current = node;
+            }
+        });
+    };
+}
+
+const handleObserver = (mutationsList, observer) => {
+    console.log(observer);
+    mutationsList.forEach((mutation) => {
+        if (mutation.addedNodes.length > 0) {
+            console.log('object');
+            const addedNodes = mutation.addedNodes[0].children;
+            for (const node of addedNodes) {
+                // if (this.#inputsToListen.includes(node.type)) {
+                // Setting which input can be empty
+                // setObjectPropertyTo(
+                //     this.options.whichInputCanBeEmpty,
+                //     node,
+                //     node.name,
+                //     'canBeEmpty',
+                //     true
+                // );
+                // Setting which input can accept special char
+                // setObjectPropertyTo(
+                //     this.options.whichInputAllowSpecialCharacters,
+                //     node,
+                //     node.name,
+                //     'allowSpecialCharacters',
+                //     true
+                // );
+                // Creating valid / invalid icon for each inputs
+                // this.#createIconContainer(node);
+                // // Main dynamic checker
+                // node.addEventListener(
+                //     'input',
+                //     debounce((e) => {
+                //         this.#dynamicCheck(e.target);
+                //     }, this.debounceDelay)
+                // );
+                // }
+            }
+        }
+    });
+};
