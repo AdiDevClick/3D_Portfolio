2bb9b0b3738d18e9a07980d419641c004ba601b4
Adrien <adrienquijo@gmail.com>
2025-04-09 12:48:21 +0200

    Ajout JSDoc

--- src/components/3DComponents/Helper.tsx
+++ src/components/3DComponents/Helper.tsx
@@ -4,16 +4,16 @@ import { AxesHelper, Mesh } from 'three';
 /**
  * Ajoute un "Helper" à l'élément 3D qui montre
  * la direction ainsi que la position de l'élément dans l'espace
- * @param size - La taille du helper
- * @param position - Montre la position
- * @param rotation- Montre la rotation
- * @param ref - La référence de l'objet 3D auquel le helper est attaché
+ * @param size - La taille du helper.
+ * @param position **@default=true** - Montre la position.
+ * @param rotation **@default=false** - Montre la rotation.
+ * @param ref **@default=2** - La référence de l'objet 3D auquel le helper est attaché.
  */
 export function Helper(
     ref: RefObject<Mesh>,
-    position: true = true,
-    rotation: false = false,
-    size: 2 = 2
+    position = true,
+    rotation = false,
+    size = 2
 ) {
     if (!ref.current) return;
 
