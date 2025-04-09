6f99fabcf26a65d1986cab19f77ad4ae28a68254
Adrien <adrienquijo@gmail.com>
2025-04-09 23:19:04 +0200

    Modification majeure du typage pour ne plus inclure les objets mais le retour des variables à la place - Les variables retourneront un nombre ou un boolean mais plus d'objet - Cela évitera des erreurs visuelles de l'IDE -

--- src/configs/3DCarouselSettingsTypes.tsx
+++ src/configs/3DCarouselSettingsTypes.tsx
@@ -1,21 +1,29 @@
 export interface CarouselGeneralSettings {
-    CONTAINER_SCALE: { value: number; min: number; max: number; step: number };
-    CARD_SCALE: { value: number; min: number; max: number; step: number };
+    CONTAINER_SCALE: number;
+    // CONTAINER_SCALE: { value: number; min: number; max: number; step: number };
+    CARD_SCALE: number;
+    // CARD_SCALE: { value: number; min: number; max: number; step: number };
     ACTIVE_CARD: boolean;
     CARD_ANIMATION: {
         value: 'Move_Out' | 'Move_In';
         options: ('Move_Out' | 'Move_In')[];
     };
-    CARDS_COUNT: { value: number; min: number; max: number; step: number };
-    CARD_SPEED: { value: number; min: number; max: number; step: number };
+    CARDS_COUNT: number;
+    // CARDS_COUNT: { value: number; min: number; max: number; step: number };
+    CARD_SPEED: number;
 }
+//     CARD_SPEED: { value: number; min: number; max: number; step: number };
+// }
 
 export interface BoundariesSettings {
     attachCamera: boolean;
     debug: boolean;
-    x: { value: number; min: number; max: number };
-    y: { value: number; min: number; max: number };
-    z: { value: number; min: number; max: number };
+    x: number;
+    // x: { value: number; min: number; max: number };
+    y: number;
+    // y: { value: number; min: number; max: number };
+    z: number;
+    // z: { value: number; min: number; max: number };
     path: {
         value: 'Circle' | 'Rollercoaster' | 'Infinity' | 'Heart';
         options: ('Circle' | 'Rollercoaster' | 'Infinity' | 'Heart')[];
@@ -23,16 +31,21 @@ export interface BoundariesSettings {
 }
 
 export interface CardsSettings {
-    THREED: { value: boolean };
-    ALIGNMENT: { value: boolean };
-    BENDING: { value: number; min: number; max: number; step: number };
-    y_HEIGHT: { value: number; min: number; max: number; step: number };
+    // THREED: { value: boolean };
+    THREED: boolean;
+    ALIGNMENT: boolean;
+    // ALIGNMENT: { value: boolean };
+    BENDING: number;
+    // BENDING: { value: number; min: number; max: number; step: number };
+    y_HEIGHT: number;
+    // y_HEIGHT: { value: number; min: number; max: number; step: number };
 }
 
 export interface PresenceSettings {
     PRESENCE_CIRCLE: boolean;
     COLLISIONS: boolean;
-    PRESENCE_RADIUS: { value: number; min: number; max: number; step: number };
+    PRESENCE_RADIUS: number;
+    // PRESENCE_RADIUS: { value: number; min: number; max: number; step: number };
     CARD_WIREFRAME: boolean;
 }
 
