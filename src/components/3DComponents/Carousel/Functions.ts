ab8b5c17dfab4635f06c9b84570309306baedb4d
Adrien <adrienquijo@gmail.com>
2025-04-11 10:03:24 +0200

    Les pointer events ont été intégrés dans le fichier - Ajout de fonctions pour les animations de la cartes pour les status inactif/clicked/active

--- src/components/3DComponents/Carousel/Functions.ts
+++ src/components/3DComponents/Carousel/Functions.ts
@@ -2,10 +2,13 @@
  * CAROUSEL FUNCTIONS
  */
 
+import { CardProps } from '@/components/3DComponents/Carousel/FunctionsTypes.ts';
 import { TWO_PI } from '@/configs/3DCarousel.config.ts';
 import { SettingsType } from '@/configs/3DCarouselSettingsTypes.tsx';
-import { ElementType } from '@/hooks/reducers/carouselTypes.ts';
-import { Vector3 } from 'three';
+import { ElementType, ReducerType } from '@/hooks/reducers/carouselTypes.ts';
+import { ThreeEvent } from '@react-three/fiber';
+import { easing } from 'maath';
+import { Euler, Vector3 } from 'three';
 import { randFloat } from 'three/src/math/MathUtils.js';
 
 /**
@@ -61,3 +64,134 @@ export function createCardProperties(
         cardAngles,
     };
 }
+
+/**
+ * Gère le clic sur une carte
+ */
+export function onClickHandler(
+    e: ThreeEvent<MouseEvent>,
+    card: ElementType,
+    reducer: ReducerType
+): void {
+    e.stopPropagation();
+    console.log(card);
+    // Deny any other clicked elements if one is opened
+    if (reducer.activeContent?.id === card.id) reducer.clickElement(card);
+    if (!reducer.activeContent) reducer.clickElement(card);
+    // reducer.animate(card, 'dropoff');
+    // navigate('/error/page');
+    // return <Navigate to={'/error/page'} replace={true} />;
+}
+
+/**
+ * Gère la sortie du pointeur d'une carte
+ */
+export function onPointerOut(
+    e: ThreeEvent<PointerEvent>,
+    card: ElementType,
+    reducer: ReducerType
+): void {
+    e.stopPropagation();
+    if (reducer.activeContent?.isClicked) return;
+    reducer.activateElement(card, false);
+}
+
+/**
+ * Gère le hover d'une carte
+ */
+export function onHover(
+    e: ThreeEvent<PointerEvent>,
+    card: ElementType,
+    reducer: ReducerType
+): void {
+    e.stopPropagation();
+    if (reducer.activeContent?.isClicked) return;
+    reducer.activateElement(card, true);
+}
+
+/**
+ * Gère les animations d'une carte par défaut
+ */
+export function handleNormalAnimation(
+    material: any,
+    rotation: Euler,
+    cardHoverScale: number,
+    cardHoverRadius: number,
+    cardHoverZoom: number,
+    isActive: boolean,
+    baseScale: number,
+    maxBending: number,
+    { ...props }: CardProps
+) {
+    // // Scale the card size up
+    easing.damp3(props.scale, cardHoverScale, 0.15, props.delta);
+    // Modify card radius to be rounder
+    easing.damp(material, 'radius', cardHoverRadius, 0.2, props.delta);
+    // Scale the zoom inside the plane mesh
+    easing.damp(material, 'zoom', cardHoverZoom, 0.2, props.delta);
+    // Rotation reset
+    easing.damp(rotation, 'x', 0, 0.15, props.delta);
+
+    if (!isActive && props.width > baseScale) {
+        props.setWidth((prev) => prev - props.delta);
+    }
+
+    if (!isActive && props.bending < maxBending) {
+        props.setBending((prev) => prev + props.delta);
+    }
+}
+
+/**
+ * Gère les effets d'une carte cliquée
+ */
+export function handleClickedCardEffects(
+    baseScale: number,
+    { ...props }: CardProps
+) {
+    // Scale up animation
+    const targetScale = new Vector3(1.5, 1.5, 1.5);
+    easing.damp3(props.scale, targetScale, 0.15, props.delta);
+    props.scale.lerp(targetScale, 0.1);
+
+    // Bending effect disabled
+    if (props.bending > 0) props.setBending((prev) => prev - props.delta);
+    // Zoom Bounce effect
+    if (props.width < baseScale + 0.4) {
+        props.setWidth((prev) => prev + props.delta);
+    }
+}
+
+/**
+ * Gère les effets d'une carte active
+ */
+export function handleActiveCardEffects(
+    baseScale: number,
+    { ...props }: CardProps
+): void {
+    // Remove bending effect when active
+    if (props.bending > 0) {
+        props.setBending((prev) => prev - props.delta);
+    }
+
+    // Increase width with bounce effect
+    if (props.width < baseScale + 0.2) {
+        props.setWidth((prev) => prev + props.delta);
+        // Boucing effect
+        // easing.damp3(props.scale, 0.5, 0.15, props.delta);
+    }
+}
+
+// const throttledUpdatedScale = useMemo(
+//     () =>
+//         throttle((newScale: number) => {
+//             reducer.updateScale(card, newScale);
+//         }, 100),
+//     [reducer, card]
+// );
+
+// const throttledUpdatedPosition = useMemo(() => {
+//     getSidesPositions(card, cardRef);
+//     throttle((newScale: number) => {
+//         reducer.updatePosition(card, newScale);
+//     }, 100);
+// }, [reducer, card]);
