a8c0082c0c07db298c299baf69e80e84de178ded
Adrien <adrienquijo@gmail.com>
2025-04-11 21:15:38 +0200

    Code cleanup

--- src/configs/3DCarouselSettingsTypes.tsx
+++ src/configs/3DCarouselSettingsTypes.tsx
@@ -1,29 +1,21 @@
 export interface CarouselGeneralSettings {
     CONTAINER_SCALE: number;
-    // CONTAINER_SCALE: { value: number; min: number; max: number; step: number };
     CARD_SCALE: number;
-    // CARD_SCALE: { value: number; min: number; max: number; step: number };
     ACTIVE_CARD: boolean;
     CARD_ANIMATION: {
         value: 'Move_Out' | 'Move_In';
         options: ('Move_Out' | 'Move_In')[];
     };
     CARDS_COUNT: number;
-    // CARDS_COUNT: { value: number; min: number; max: number; step: number };
     CARD_SPEED: number;
 }
-//     CARD_SPEED: { value: number; min: number; max: number; step: number };
-// }
 
 export interface BoundariesSettings {
     attachCamera: boolean;
     debug: boolean;
     x: number;
-    // x: { value: number; min: number; max: number };
     y: number;
-    // y: { value: number; min: number; max: number };
     z: number;
-    // z: { value: number; min: number; max: number };
     path: {
         value: 'Circle' | 'Rollercoaster' | 'Infinity' | 'Heart';
         options: ('Circle' | 'Rollercoaster' | 'Infinity' | 'Heart')[];
@@ -31,21 +23,16 @@ export interface BoundariesSettings {
 }
 
 export interface CardsSettings {
-    // THREED: { value: boolean };
     THREED: boolean;
     ALIGNMENT: boolean;
-    // ALIGNMENT: { value: boolean };
     BENDING: number;
-    // BENDING: { value: number; min: number; max: number; step: number };
     y_HEIGHT: number;
-    // y_HEIGHT: { value: number; min: number; max: number; step: number };
 }
 
 export interface PresenceSettings {
     PRESENCE_CIRCLE: boolean;
     COLLISIONS: boolean;
     PRESENCE_RADIUS: number;
-    // PRESENCE_RADIUS: { value: number; min: number; max: number; step: number };
     CARD_WIREFRAME: boolean;
 }
 
