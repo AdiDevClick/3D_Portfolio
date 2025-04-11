87934a2b06db63bdff5f9695d42d57dbaa4afb22
Adrien <adrienquijo@gmail.com>
2025-04-11 20:08:15 +0200

    création d'un composant sphere presence representant un helper

---
+++ src/components/3DComponents/SpherePresence/SpherePresence.tsx
@@ -0,0 +1,28 @@
+type SpherePresenceTypes = {
+    position: [number, number, number];
+    radius: [number, number];
+    color: string;
+    visible?: boolean;
+};
+
+/**
+ * Wireframe sphere presence Helper -
+ */
+export function SpherePresenceHelper({
+    position,
+    radius,
+    color,
+    visible = false,
+}: SpherePresenceTypes) {
+    return (
+        <mesh position={position} visible={visible}>
+            <sphereGeometry args={radius} />
+            <meshStandardMaterial
+                color={color}
+                wireframe
+                transparent
+                opacity={0.5}
+            />
+        </mesh>
+    );
+}
