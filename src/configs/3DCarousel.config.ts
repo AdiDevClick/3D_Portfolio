62e0321e45b25406132e8a904ebf9d0e646bbfe1
Adrien <adrienquijo@gmail.com>
2025-04-16 09:48:50 +0200

    Ajout de vecteur à l'array de positions pour que la transition puisse se faire

--- src/configs/3DCarousel.config.ts
+++ src/configs/3DCarousel.config.ts
@@ -90,9 +90,9 @@ export const DESKTOP_HTML_CONTAINER_ROTATION = [0, 0, 0];
  */
 
 /** Default carousel position on loading page */
+export const DEFAULT_CARD_POSITION = new Vector3(0, -100, 0);
 export const DEFAULT_PROJECTS_POSITION = new Vector3(0, -100, 0);
-// export const DEFAULT_PROJECTS_POSITION = new Vector3(0, -100, 0);
-export const ACTIVE_PROJECTS_POSITION = [0, 0.15, 0];
+export const ACTIVE_PROJECTS_POSITION = new Vector3(0, 0.15, 0);
 
 /** Default camera position on loading page */
 export const DEFAULT_CAMERA_POSITION = new Vector3(0, 0, -20);
