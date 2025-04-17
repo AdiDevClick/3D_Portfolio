ce9f4840835e112f7ab3b2cf9647d90baefd08dd
Adrien <adrienquijo@gmail.com>
2025-04-17 15:11:59 +0200

    mise au rebus

---
+++ src/hooks/TODELETE/useSettings_copy.tsx
@@ -0,0 +1,62 @@
+import {
+    boundariesOptions,
+    cardsSettings,
+    carouselGeneralSettings,
+    presenceSettings,
+} from '@/configs/3DCarousel.config.ts';
+import { SettingsType } from '@/configs/3DCarouselSettingsTypes.tsx';
+import { useControls } from 'leva';
+
+/**
+ * Ajoute un panel de contrôle contenant différentes sections.
+ */
+export function useSettings(datas: []): SettingsType {
+    /**
+     * Boundaries Settings
+     */
+    const { ...BOUDARIES_RULES } = useControls(
+        'Boundaries',
+        boundariesOptions,
+        { collapsed: true }
+    );
+
+    /**
+     * Carousel settings
+     */
+    const [{ ...SETTINGS }, set] = useControls(
+        'Carousel Settings',
+        () => ({
+            // !! IMPORTANT !! Override cards count with datas.length
+            ...carouselGeneralSettings,
+            CARDS_COUNT: {
+                ...carouselGeneralSettings.CARDS_COUNT,
+                value: datas.length,
+            },
+        }),
+        { collapsed: true }
+    );
+
+    /**
+     * Card Rules Settings
+     */
+    const { ...CARD_RULES } = useControls('Card Rules', cardsSettings, {
+        collapsed: true,
+    });
+
+    /**
+     * Collision Detection Settings
+     */
+    const { ...COLLISIONS_RULES } = useControls(
+        'Presence Area',
+        presenceSettings,
+        { collapsed: true }
+    );
+    console.log('je crer mes settings plusieur fois');
+    return {
+        ...SETTINGS,
+        set,
+        ...COLLISIONS_RULES,
+        ...CARD_RULES,
+        ...BOUDARIES_RULES,
+    };
+}
