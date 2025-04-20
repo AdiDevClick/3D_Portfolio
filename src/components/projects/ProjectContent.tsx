64f2b830b6b8062d71b8dad9d678cd5d6eff42f1
Adrien <adrienquijo@gmail.com>
2025-04-20 20:00:54 +0200

    Modification du Content pour ajouter les liens git et web

--- src/components/projects/ProjectContent.tsx
+++ src/components/projects/ProjectContent.tsx
@@ -1,6 +1,8 @@
+import { Tags } from '@/components/tags/Tags.tsx';
 import { TagsContainer } from '@/components/tags/TagsContainer.tsx';
 import { ElementType } from '@/hooks/reducers/carouselTypes.ts';
-import starIcon from '@icons/star.svg';
+import { useId } from 'react';
+import { Link } from 'react-router';
 
 type cardType = {
     card: ElementType;
@@ -10,27 +12,38 @@ type cardType = {
  * Le contenu des projets
  */
 export function ProjectContent({ card }: cardType) {
+    Array.isArray(card.stack) ? card.stack : [card.stack];
     return (
         <>
             <span className="card__close"></span>
             {/* <img src={card.url} alt="Preview Project" /> */}
             <h2 className="card__title">{card.title}</h2>
-            {/* <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Cupiditate doloribus ex quam repellendus assumenda eligendi nam
-                rerum aut sequi nulla itaque atque, et neque, ea minima magni
-                fugit sunt quibusdam! Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Magni commodi fuga sed asperiores neque amet
-                corporis in facilis, aliquam hic? Esse ea ipsa magni modi! Unde
-                totam quo quos ullam. Lorem, ipsum dolor sit amet consectetur
-                adipisicing elit. Maiores delectus et illum placeat ea, dolore
-                accusamus animi, labore saepe quas architecto corrupti, quo
-                nulla ullam nesciunt cum nisi voluptatem tempore. Lorem ipsum
-                dolor sit amet consectetur adipisicing elit. Placeat nisi at quo
-                natus odit consectetur eius exercitationem, assumenda ducimus
-                sunt dolores voluptatem molestias, cum nulla suscipit magni
-                possimus! Harum, libero.
-            </p> */}
+            {/* <button>Lien GitHub</button>
+            <button>Lien Website</button> */}
+            <TagsContainer>
+                {card.links.map((element) => {
+                    const id = useId();
+                    return (
+                        <a
+                            key={id}
+                            href={element.link}
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <Tags logo={element.logo}>{element.name}</Tags>
+                        </a>
+                    );
+                })}
+                {/* {Object.entries([card.links]).map((link, index) => {
+                    console.log(link.name);
+                    const id = useId();
+                    return (
+                        <Link key={id} to={link.link} target="_blank">
+                            <Tags logo={link.logo}>{link.name}</Tags>
+                        </Link>
+                    );
+                })} */}
+            </TagsContainer>
             <p className="card__description">{card.description}</p>
             <ul className="card__content">
                 {card.content.map((text, index) => (
@@ -47,7 +60,16 @@ export function ProjectContent({ card }: cardType) {
                     </li>
                 ))}
             </ul>
-            <TagsContainer datas={card.stack} />
+            <TagsContainer>
+                {Object.entries(card.stack).map(([text, logoUrl]) => {
+                    const id = useId();
+                    return (
+                        <Tags key={id} logo={logoUrl}>
+                            {text}
+                        </Tags>
+                    );
+                })}
+            </TagsContainer>
         </>
     );
 }
