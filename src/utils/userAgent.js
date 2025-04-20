ec78ff14fb62edd8c89af2469da44da6c04451b9
Adrien <adrienquijo@gmail.com>
2025-04-20 19:19:46 +0200

    Ajout d'un fichier contenant des fonctions pouvant vérifier si l'utilisateur est sur iPad

---
+++ src/utils/userAgent.js
@@ -0,0 +1,33 @@
+const userAgent = navigator.userAgent;
+
+/**
+ * Vérifie que l'utilisateur est sur iPad et si sa version est compatible
+ * @param {number} maxVersion Le numéro de version iOS à tester
+ * @returns
+ */
+export function isIPadWithiOSVersion(maxVersion) {
+    // Vérifiez si c'est un iPad
+    const isIPadBrowser = /iPad/.test(userAgent);
+    const isIPadDevice = /Macintosh/i.test(navigator.userAgent);
+    // Vérifiez la version d'iOS
+    const iOSVersionMatch = userAgent.match(/OS (\d+)_\d+/);
+    if (iOSVersionMatch) {
+        const iOSVersion = parseInt(iOSVersionMatch[1], 10);
+        return (isIPadBrowser || isIPadDevice) && iOSVersion <= maxVersion;
+    }
+    return false;
+}
+
+/**
+ * Vérifie si l'utilisateur utilise un iPad
+ * @returns
+ */
+export function isIPad() {
+    // Vérifiez si c'est un iPad
+    const isIPadBrowser = /iPad/.test(userAgent);
+    const isIPadDevice = /Macintosh/i.test(navigator.userAgent);
+    if ((isIPadDevice || isIPadBrowser) && navigator.maxTouchPoints) {
+        return true;
+    }
+    return false;
+}
