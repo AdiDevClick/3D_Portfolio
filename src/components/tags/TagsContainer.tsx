1190f90cb2f0c558349bbb535b6a92888d7cca46
Adrien <adrienquijo@gmail.com>
2025-04-20 19:26:53 +0200

    Modification du tag container pour accepter un children

--- src/components/tags/TagsContainer.tsx
+++ src/components/tags/TagsContainer.tsx
@@ -1,6 +1,5 @@
-import { Tags } from '@/components/tags/Tags.tsx';
 import { ElementType } from '@/hooks/reducers/carouselTypes.ts';
-import { useId } from 'react';
+import { PropsWithChildren } from 'react';
 import '@css/Tags.scss';
 type DatasType = {
     /** La propriété "stack" de la card contenant toute la stack technique */
@@ -10,18 +9,6 @@ type DatasType = {
 /**
  * Crer des tags puis les affiches dans un container
  */
-export function TagsContainer({ datas }: DatasType) {
-    Array.isArray(datas) ? datas : [datas];
-    return (
-        <ul className="tags_container">
-            {Object.entries(datas).map(([text, logoUrl]) => {
-                const id = useId();
-                return (
-                    <Tags key={id} logo={logoUrl}>
-                        {text}
-                    </Tags>
-                );
-            })}
-        </ul>
-    );
+export function TagsContainer({ children }: PropsWithChildren) {
+    return <ul className="tags_container">{children}</ul>;
 }
