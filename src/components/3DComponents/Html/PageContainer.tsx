97ea9c8008f831e3537ca8f5f777c652b118fd97
Adrien <adrienquijo@gmail.com>
2025-04-25 11:39:22 +0200

    Création d'un composant PageContainer qui permettra d'éviter les doublons de pages - Il prend un children en params pour pouvoir passer le contenu des pages HMTL - Il prend en params un pageName pour pouvoir naviguer dans le content facilement - Ajout des types pour les params -

---
+++ src/components/3DComponents/Html/PageContainer.tsx
@@ -0,0 +1,60 @@
+import { HtmlContainer } from '@/components/3DComponents/Html/HtmlContainer.tsx';
+import {
+    ACTIVE_PROJECTS_POSITION_SETTINGS,
+    DEFAULT_PROJECTS_POSITION_SETTINGS,
+} from '@/configs/3DCarousel.config.ts';
+import { Billboard } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { easing } from 'maath';
+import { ReactNode, useEffect, useRef, useState } from 'react';
+import { useLocation } from 'react-router';
+import { Group } from 'three';
+
+type PageContainerTypes = { children: ReactNode; pageName: string };
+
+export function PageContainer({ children, pageName }: PageContainerTypes) {
+    const groupRef = useRef<Group>(null);
+    const location = useLocation();
+
+    const [activeURL, setActiveURL] = useState(false);
+
+    /**
+     * Activate page URL and content loaded
+     */
+    useEffect(() => {
+        if (!groupRef.current) return;
+        location.pathname === pageName
+            ? setActiveURL(true)
+            : setActiveURL(false);
+    }, [location]);
+
+    /**
+     * POSITIONING IF URL IS ACTIVE / NON ACTIVE -
+     */
+    useFrame((_, delta) => {
+        if (groupRef.current) {
+            easing.damp3(
+                groupRef.current.position,
+                activeURL
+                    ? ACTIVE_PROJECTS_POSITION_SETTINGS
+                    : DEFAULT_PROJECTS_POSITION_SETTINGS,
+                0.3,
+                delta
+            );
+        }
+    });
+
+    return (
+        <Billboard
+            ref={groupRef}
+            follow={true}
+            lockX={false}
+            lockY={false}
+            lockZ={false}
+        >
+            <HtmlContainer key={pageName} className="html-container">
+                {children}
+            </HtmlContainer>
+        </Billboard>
+    );
+}
