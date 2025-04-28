597035141a93ee65e1c08fa5db904aa365677ab1
Adrien <adrienquijo@gmail.com>
2025-04-28 10:28:06 +0200

    Code Cleanup - Ajout de la classe home à la page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,5 @@
-import { HtmlContainer } from '@/components/3DComponents/Html/HtmlContainer.tsx';
 import { PageContainer } from '@/components/3DComponents/Html/PageContainer.tsx';
-import {
-    ACTIVE_PROJECTS_POSITION_SETTINGS,
-    DEFAULT_PROJECTS_POSITION_SETTINGS,
-} from '@/configs/3DCarousel.config.ts';
 import { HomeContent } from '@/pages/Home/HomeContent.tsx';
-import { Billboard } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
-import { easing } from 'maath';
-import { useEffect, useRef, useState } from 'react';
-import { useLocation } from 'react-router';
-import { Group } from 'three';
 
 const handleObserver = (mutationsList, observer) => {
     console.log('Mutation détectée:', mutationsList.length);
@@ -52,42 +41,10 @@ const handleObserver = (mutationsList, observer) => {
 };
 
 export function Home() {
-    const pageRef = useRef(null);
-    // const groupRef = useRef<Group>(null);
-    // const location = useLocation();
-
-    // const [isLoaded, setIsLoaded] = useState(false);
-    // const [activeURL, setActiveURL] = useState(false);
-
-    // /**
-    //  * Activate page URL and content loaded
-    //  */
-    // useEffect(() => {
-    //     if (!pageRef.current) return;
-    //     setIsLoaded(true);
-    //     location.pathname === '/' ? setActiveURL(true) : setActiveURL(false);
-    // }, [pageRef.current, location]);
-
-    // /**
-    //  * POSITIONING IF URL IS ACTIVE / NON ACTIVE -
-    //  */
-    // useFrame((_, delta) => {
-    //     if (groupRef.current && isLoaded) {
-    //         easing.damp3(
-    //             groupRef.current.position,
-    //             activeURL
-    //                 ? ACTIVE_PROJECTS_POSITION_SETTINGS
-    //                 : DEFAULT_PROJECTS_POSITION_SETTINGS,
-    //             0.3,
-    //             delta
-    //         );
-    //     }
-    // });
-
     return (
         <PageContainer pageName={'/'}>
             <HomeContent
-                ref={pageRef}
+                className="home"
                 style={{
                     // opacity: isLoaded ? 1 : 0,
                     transform: 'translate(-50%)',
@@ -100,28 +57,5 @@ export function Home() {
                 }}
             />
         </PageContainer>
-        // <Billboard
-        //     ref={groupRef}
-        //     follow={true}
-        //     lockX={false}
-        //     lockY={false}
-        //     lockZ={false}
-        // >
-        //     <HtmlContainer key={'home'} className="html-container">
-        //         <HomeContent
-        //             ref={pageRef}
-        //             style={{
-        //                 opacity: isLoaded ? 1 : 0,
-        //                 transform: 'translate(-50%)',
-        //                 transition:
-        //                     'opacity 0.5s ease-in-out, transform 0.5s ease-in-out',
-        //                 height: '500px',
-        //                 width: 'clamp(min(52%, 100%), 100%, 52vw)',
-        //                 background: 'rgba(255, 255, 255, 0.95)',
-        //                 zIndex: 0,
-        //             }}
-        //         />
-        //     </HtmlContainer>
-        // </Billboard>
     );
 }
