b7d21151a0fa7e99bc97c4f31c938efe12f6b2d4
Adrien <adrienquijo@gmail.com>
2025-04-28 10:28:28 +0200

    Déplacement des composants

---
+++ src/pages/Projects/ProjectContainer.tsx
@@ -0,0 +1,40 @@
+import { ProjectContent } from '@/pages/Projects/ProjectContent';
+import { ElementType } from '@/hooks/reducers/carouselTypes.ts';
+import { WheelEvent } from 'react';
+import '@css/Card.scss';
+
+type ProjectContainerTypes = {
+    onClick: () => void;
+    card: ElementType;
+};
+
+export function ProjectContainer({
+    onClick,
+    card,
+    ...props
+}: ProjectContainerTypes) {
+    /**
+     * Réactive le scrolling quand le contenu est ouvert
+     */
+    const onScrollHandler = (e: WheelEvent) => {
+        const { scrollTop, scrollHeight, clientHeight } = e.currentTarget;
+        if (
+            (scrollTop + e.deltaY > 0 && e.deltaY < 0) ||
+            (scrollHeight - (scrollTop + e.deltaY) > clientHeight &&
+                e.deltaY > 0)
+        ) {
+            e.stopPropagation();
+        }
+    };
+
+    return (
+        <div
+            className="card"
+            onClick={onClick}
+            onWheel={onScrollHandler}
+            {...props}
+        >
+            <ProjectContent card={card} {...props} />
+        </div>
+    );
+}
