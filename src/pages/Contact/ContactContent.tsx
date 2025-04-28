5700a1dcf4ebc11a0900984a7c6f7002445ad36e
Adrien <adrienquijo@gmail.com>
2025-04-28 10:27:47 +0200

    Création du composant ContactContent

---
+++ src/pages/Contact/ContactContent.tsx
@@ -0,0 +1,14 @@
+/**
+ * Contenu HTML de la page contact
+ */
+export function ContactContent({ ...props }) {
+    return (
+        <div {...props}>
+            <h1 style={{ color: 'black' }}>Me Contacter</h1>
+            <p style={{ color: 'black' }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, voluptatibus. Lorem ipsum dolor sit amet{' '}
+            </p>
+        </div>
+    );
+}
