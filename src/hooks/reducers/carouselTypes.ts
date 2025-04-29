6500977461ef62c5a83d89243fb31e487df761ee
Adrien <adrienquijo@gmail.com>
2025-04-29 16:46:39 +0200

    Modification du type pour le CardContentType links qui ne contient plus "url" mais "link" dans l'objet Links

--- src/hooks/reducers/carouselTypes.ts
+++ src/hooks/reducers/carouselTypes.ts
@@ -32,7 +32,7 @@ interface CardContentType {
     /** The description of the project next to the title of the card content */
     description: string;
     /** The links of the project */
-    links: { name: string; url: string; logo: string }[];
+    links: { name: string; link: string; logo: string }[];
 }
 
 type typeAngles = { active: number; onHold: number };
