ed0b0d4a58f8184cf8d942c07c839adcb58d6f7b
Adrien <adrienquijo@gmail.com>
2025-04-30 19:55:07 +0200

    Le composant a été déplacé pour être tout seul dans un fichier

---
+++ src/components/3DComponents/3DIcons/IconMesh.tsx
@@ -0,0 +1,31 @@
+import { Object3D } from 'three';
+import { JSX } from 'react';
+
+/**
+ * Creates a 3D icon mesh component
+ *
+ * @param iconColor - Color of the icon
+ * @param data - Mesh data to be used
+ */
+export function IconMesh({
+    data,
+    iconColor,
+    hovered,
+    curveSegments,
+    ...props
+}: {
+    data: Object3D;
+    iconColor: string;
+    hovered?: boolean;
+    curveSegments?: number;
+} & JSX.IntrinsicElements['mesh']) {
+    return (
+        <mesh {...data} {...props}>
+            <meshStandardMaterial
+                color={hovered ? '#4285F4' : iconColor}
+                emissive={hovered ? '#4285F4' : '#000000'}
+                emissiveIntensity={hovered ? 0.3 : 0}
+            />
+        </mesh>
+    );
+}
