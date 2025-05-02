b732490c734e86a9f52ed5ae213f5727be1f5a55
Adrien <adrienquijo@gmail.com>
2025-05-02 22:38:37 +0200

    Modification de la position de la stack technique - Les modèles 3D étant créé, utilisation du fichier d'icônes original pour pouvoir les utiliser -

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,14 +8,15 @@ import {
 } from '@/configs/3DCarousel.config.ts';
 import { ReducerType } from '@/hooks/reducers/carouselTypes.ts';
 import { HomeContent } from '@/pages/Home/HomeContent.tsx';
-import { CenterProps, Float } from '@react-three/drei';
+import { CenterProps, Float, Scroll } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { easing } from 'maath';
 import { useMemo, useRef } from 'react';
 import { Vector3 } from 'three';
 import { Group } from 'three/examples/jsm/libs/tween.module.js';
-import iconsWithText from '@data/techstacktest.json';
-// import iconsWithText from '@data/techstack.json';
+// import iconsWithText from '@data/techstacktest.json';
+import iconsWithText from '@data/techstack.json';
+import { HtmlContainer } from '@/components/3DComponents/Html/HtmlContainer.tsx';
 
 type HomeTypes = {
     reducer: ReducerType;
@@ -45,15 +46,19 @@ export function Home({ reducer, margin = 0.5 }: HomeTypes) {
     );
 
     if (isActive) {
-        titlePosition = new Vector3(0, 0, 0);
-        stackPosition = new Vector3(
-            0,
-            -contentHeight * generalScaleX + margin,
-            0
-        );
+        titlePosition = [0, 0, 0];
+        // titlePosition = new Vector3(0, 0, 0);
+        stackPosition = [0, -contentHeight * generalScaleX + margin, 0];
+        // stackPosition = new Vector3(
+        //     0,
+        //     -contentHeight * generalScaleX + margin,
+        //     0
+        // );
     } else {
-        titlePosition = DEFAULT_PROJECTS_POSITION_SETTINGS;
-        stackPosition = DEFAULT_PROJECTS_POSITION_SETTINGS;
+        titlePosition = [0, -100, 0];
+        // titlePosition = DEFAULT_PROJECTS_POSITION_SETTINGS;
+        stackPosition = [0, -100, 0];
+        // stackPosition = DEFAULT_PROJECTS_POSITION_SETTINGS;
     }
 
     useFrame((_, delta) => {
@@ -67,7 +72,31 @@ export function Home({ reducer, margin = 0.5 }: HomeTypes) {
     });
 
     return (
-        <group ref={groupRef} renderOrder={-100}>
+        <group ref={groupRef}>
+            {/* <group ref={groupRef} renderOrder={-100}> */}
+
+            {/* <HtmlContainer className="html-container">
+                <div
+                    // className="about"
+                    id="scroll-container"
+                    style={{
+                        height: '1000vh',
+                        width: '100vw',
+                        // overflow: 'auto',
+                        WebkitOverflowScrolling: 'touch', // Important pour iOS
+                        position: 'fixed',
+                        scale: 4,
+                        // inset: 0,
+                        backgroundColor: 'black',
+                        // transform: 'translate(-50%)',
+                        // zIndex: 10000000000,
+                        // pointerEvents: reducer.activeContent?.isClicked
+                        //     ? 'none'
+                        //     : 'auto',
+                    }}
+                ></div>
+            </HtmlContainer> */}
+
             <group ref={titleRef}>
                 <Float position-y={1 * generalScaleX} {...floatOptions}>
                     <Title
@@ -132,9 +161,11 @@ export function Home({ reducer, margin = 0.5 }: HomeTypes) {
                     </Title>
                 </Float>
                 <IconsContainer
-                    rotation={[0, 3.164, 0]}
+                    // rotation={[0, 3.164, 0]}
+                    width={contentWidth}
                     icons={iconsWithText}
                     scalar={generalScaleX}
+                    position-y={-1 * generalScaleX - margin}
                 />
             </group>
         </group>
