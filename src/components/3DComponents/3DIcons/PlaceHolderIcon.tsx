5a642b6800f6305981b33e7ab3b69a0694f36567
Adrien <adrienquijo@gmail.com>
2025-05-05 09:16:33 +0200

    Création d'un PlaceholderIcon pour le loading de l'app - Il invoque le HexCell

---
+++ src/components/3DComponents/3DIcons/PlaceHolderIcon.tsx
@@ -0,0 +1,18 @@
+import { HexCell } from '@/components/3DComponents/Forms/HexCell.tsx';
+import { Center } from '@react-three/drei';
+
+/**
+ * PlaceholderIcon component for loading state.
+ *
+ * @param props - Props for the PlaceholderIcon component
+ * @returns A placeholder icon component that displays a hex cell in the center.
+ */
+export function PlaceholderIcon({ ...props }) {
+    return (
+        <group {...props}>
+            <Center>
+                <HexCell scalar={0.5} />
+            </Center>
+        </group>
+    );
+}
