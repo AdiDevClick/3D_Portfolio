32d280b7116ea9857e13f740733ab29e8368516e
Adrien <adrienquijo@gmail.com>
2025-05-06 22:24:18 +0200

    Le useFrame du carousel ne s'activera que quand le carousel est visible - code cleanup - Certaines variables ont été sorties du composant pour éviter la forte utilisation de mémoire - Le calcul du useframe a été mémoizé - Modification dans l'ordre d'apparition des cartes - Suppression des fallbacks - Le carousel ne sera plus rendered quand il n'est plus actif -

--- src/components/3DComponents/Carousel/Carousel.tsx
+++ src/components/3DComponents/Carousel/Carousel.tsx
@@ -1,5 +1,5 @@
-import { Suspense, useEffect, useId, useMemo, useRef, useState } from 'react';
-import { DoubleSide, Vector3 } from 'three';
+import { useEffect, useId, useMemo, useRef, useState } from 'react';
+import { DoubleSide, Mesh, Vector3 } from 'three';
 import { useFrame } from '@react-three/fiber';
 import { easing } from 'maath';
 import { SettingsType } from '@/configs/3DCarouselSettingsTypes.tsx';
@@ -16,10 +16,19 @@ import { ReducerType } from '@/hooks/reducers/carouselTypes.ts';
 import { useLocation } from 'react-router';
 import { Title } from '@/components/3DComponents/Title/Title.tsx';
 import { Float } from '@react-three/drei';
-import { Group } from 'three/examples/jsm/libs/tween.module.js';
-import { PlaceholderIcon } from '@/components/3DComponents/3DIcons/PlaceHolderIcon.tsx';
+import { Group } from 'three';
 
 const activeForwardOffset = 0.5;
+const animationSpeed = 0.22;
+
+let animationProgress = 0;
+
+const startPos = new Vector3();
+const targetPos = new Vector3();
+const midPoint = new Vector3();
+const bezierPos = new Vector3();
+
+let frameCountRef = 0;
 
 interface CarouselProps {
     reducer: ReducerType;
@@ -32,43 +41,28 @@ export default function Carousel({
     datas,
     reducer,
     SETTINGS,
-    loadingStage,
-    numberOfCardsToShow,
 }: CarouselProps) {
-    // const { boundaries, datas, reducer, SETTINGS } = useOutletContext();
+    const {
+        contentHeight,
+        activeContent,
+        showElements,
+        isMobile,
+        loadedCount,
+    } = reducer;
 
-    const segmentsCount = reducer.isMobile ? 6 : 16;
-    const textureQuality = reducer.isMobile ? 512 : 1024;
-    const animationSpeed = reducer.isMobile ? 0.8 : 1;
-
-    const frameCountRef = useRef(0);
     const projectsRef = useRef<Group>(null!);
     const titleRef = useRef<Group>(null!);
-
-    // Animations Refs
-    const startPosRef = useRef(new Vector3());
-    const targetPosRef = useRef(new Vector3());
-    const midPointRef = useRef(new Vector3());
-    const bezierPosRef = useRef(new Vector3());
+    const boundariesRef = useRef<Mesh>(null!);
 
     const location = useLocation();
     const id = useId();
 
     // États pour l'animation
     const [isAnimatingIn, setIsAnimatingIn] = useState(false);
-    const [animationProgress, setAnimationProgress] = useState(0);
-    const [wasActive, setWasActive] = useState(false);
-    const [shouldBreathe, setShouldBreathe] = useState(false);
     const [isInitialLoading, setIsInitialLoading] = useState(true);
 
     const activeURL = location.pathname.includes('projets');
     const activeProject = location.pathname.split('/')[2];
-
-    useEffect(() => {
-        // Après le premier rendu, désactiver le mode chargement simplifié
-        const timer = setTimeout(() => setIsInitialLoading(false), 1500);
-        return () => clearTimeout(timer);
-    }, []);
 
     /**
      * Création des propriétés des cartes -
@@ -77,7 +71,7 @@ export default function Carousel({
         return new Array(SETTINGS.CARDS_COUNT)
             .fill(null)
             .map((_, i, self) =>
-                createCardProperties(SETTINGS, datas, i, self, id)
+                createCardProperties(SETTINGS, datas, i, self, id, isMobile)
             );
     }, [
         SETTINGS.CARDS_COUNT,
@@ -88,12 +82,53 @@ export default function Carousel({
         datas,
     ]);
 
+    const cardPositionsMemo = useMemo(() => {
+        if (!showElements.length)
+            return {
+                itemPositions: {} as Record<
+                    string,
+                    ReturnType<typeof calculateCardPositions>
+                >,
+                activeCard: -1,
+            };
+
+        const activeCard = showElements.findIndex(
+            (el) => el.isActive || el.isClicked
+        );
+
+        const itemPositions = showElements.reduce<
+            Record<string, ReturnType<typeof calculateCardPositions>>
+        >((acc, item, i) => {
+            // if (!item.ref) return acc;
+
+            acc[item.id] = calculateCardPositions(
+                item,
+                i,
+                activeCard,
+                showElements.length,
+                SETTINGS.CONTAINER_SCALE,
+                activeForwardOffset,
+                SETTINGS.THREED
+            );
+
+            return acc;
+        }, {});
+        return { itemPositions, activeCard };
+    }, [
+        showElements.map((el) => el.id).join(','),
+        showElements.map((el) => el.isActive || el.isClicked).join(','),
+        showElements.length,
+        SETTINGS.CONTAINER_SCALE,
+        SETTINGS.THREED,
+        activeForwardOffset,
+    ]);
+
     /**
      * Crer l'array du reducer qui sera partagé -
      * Il ne sera créé qu'une fois -
      */
     useEffect(() => {
-        const currentIds = reducer.showElements.map((el) => el.id);
+        const currentIds = showElements.map((el) => el.id);
         cardsMemo.forEach((card) => {
             if (!currentIds.includes(card.id)) {
                 reducer.addElements(card);
@@ -101,7 +136,7 @@ export default function Carousel({
                 reducer.updateElements(card);
             }
         });
-        if (cardsMemo.length < reducer.showElements.length) {
+        if (cardsMemo.length < showElements.length) {
             reducer.deleteElements(cardsMemo);
         }
     }, [cardsMemo]);
@@ -113,33 +148,26 @@ export default function Carousel({
         // Turn off animations
         if (!activeURL) {
             if (isAnimatingIn) setIsAnimatingIn(false);
-            if (reducer.activeContent) {
-                setShouldBreathe(activeURL);
-                reducer.activeContent.isActive = false;
-                reducer.activeContent.isClicked = false;
+            if (activeContent) {
+                // setShouldBreathe(activeURL);
+                activeContent.isActive = false;
+                activeContent.isClicked = false;
             }
         }
 
         // Animations init
-        if (activeURL && !wasActive && !activeProject) {
-            setShouldBreathe(activeURL);
-            setAnimationProgress(0);
+        if (activeURL && !activeProject && !isInitialLoading) {
+            // setShouldBreathe(activeURL);
+            animationProgress = 0;
             setIsAnimatingIn(true);
         }
-
-        setWasActive(activeURL);
-    }, [activeURL]);
-
-    const startPos = startPosRef.current;
-    const targetPos = targetPosRef.current;
-    const midPoint = midPointRef.current;
-    const bezierPos = bezierPosRef.current;
+    }, [activeURL, isInitialLoading]);
 
     useFrame((state, delta) => {
-        if (!projectsRef.current) return;
-        frameCountRef.current += 1;
+        if (!projectsRef.current || !projectsRef.current.visible) return;
 
-        const { contentHeight, activeContent, showElements } = reducer;
+        frameCountRef += 1;
+
         const commonParams = { animationProgress, delta };
 
         // Update carousel container
@@ -154,15 +182,15 @@ export default function Carousel({
 
         // Update title position
         updateTitlePosition(titleRef, activeURL, contentHeight ?? 0, delta);
-
         // Handle animation progress
+
         if (isAnimatingIn) {
             // !! IMPORTANT !! Disable collisions during animation
             SETTINGS.set({ COLLISIONS: false });
 
-            const animationSpeed = 0.22;
-            setAnimationProgress((prev) =>
-                Math.min(1, prev + delta * animationSpeed)
+            animationProgress = Math.min(
+                1,
+                animationProgress + delta * animationSpeed
             );
 
             if (animationProgress >= 1) {
@@ -182,37 +210,13 @@ export default function Carousel({
         }
 
         // Find active card
-        const activeCard = showElements.findIndex(
-            (el) => el.isActive || el.isClicked
-        );
+        const { itemPositions, activeCard } = cardPositionsMemo;
 
-        // Handle each card
         showElements.forEach((item, i) => {
-            if (!item.ref || !item.ref.current) return;
+            if (!item.ref?.current || !itemPositions[item.id]) return;
 
             // Calculate positions for this card
-            const { positions, targetRotationY } = calculateCardPositions(
-                item,
-                i,
-                activeCard,
-                showElements.length,
-                SETTINGS.CONTAINER_SCALE,
-                activeForwardOffset,
-                SETTINGS.THREED
-            );
-
-            // Handle collisions
-            if (activeCard === -1) {
-                handleCardCollisions(
-                    item,
-                    i,
-                    showElements,
-                    SETTINGS.CONTAINER_SCALE,
-                    frameCountRef.current,
-                    SETTINGS.COLLISIONS,
-                    SETTINGS.set
-                );
-            }
+            const { positions, targetRotationY } = itemPositions[item.id];
 
             // Handle animation during entry
             if (isAnimatingIn && activeURL) {
@@ -220,7 +224,6 @@ export default function Carousel({
                     item,
                     i,
                     showElements.length,
-                    isInitialLoading,
                     targetRotationY,
                     commonParams,
                     { startPos, targetPos, midPoint, bezierPos, positions }
@@ -229,7 +232,20 @@ export default function Carousel({
                 if (animationHandled) return;
             }
 
-            // Normal positioning
+            // Handle collisions
+            if (activeCard === -1 && SETTINGS.COLLISIONS && activeURL) {
+                handleCardCollisions(
+                    item,
+                    i,
+                    showElements,
+                    SETTINGS.CONTAINER_SCALE,
+                    frameCountRef,
+                    SETTINGS.COLLISIONS,
+                    SETTINGS.set
+                );
+            }
+            if (!activeURL) return;
+
             const { position, rotation } = item.ref.current;
 
             easing.damp3(
@@ -238,36 +254,39 @@ export default function Carousel({
                 0.15,
                 delta
             );
+
             easing.damp(rotation, 'y', targetRotationY, 0.15, delta);
         });
     });
 
+    useEffect(() => {
+        if (loadedCount >= showElements.length) {
+            setIsInitialLoading(false);
+        }
+    }, [showElements, loadedCount]);
+
     return (
-        <group ref={projectsRef}>
-            <Suspense fallback={null}>
-                <mesh visible={SETTINGS.debug}>
-                    <boxGeometry
-                        args={[boundaries.x, boundaries.y, boundaries.z]}
-                    />
-                    <meshStandardMaterial
-                        color={'orange'}
-                        transparent
-                        opacity={0.5}
-                        side={DoubleSide}
-                    />
-                </mesh>
-            </Suspense>
-            <Suspense fallback={<PlaceholderIcon />}>
-                <Float>
-                    <Title
-                        name={'carousel__title'}
-                        ref={titleRef}
-                        rotation={[0, 3.164, 0]}
-                    >
-                        Mes Projets
-                    </Title>
-                </Float>
-            </Suspense>
+        <group visible={activeURL} ref={projectsRef}>
+            <mesh ref={boundariesRef} visible={SETTINGS.debug}>
+                <boxGeometry
+                    args={[boundaries.x, boundaries.y, boundaries.z]}
+                />
+                <meshStandardMaterial
+                    color={'orange'}
+                    transparent
+                    opacity={0.5}
+                    side={DoubleSide}
+                />
+            </mesh>
+            <Float>
+                <Title
+                    name={'carousel__title'}
+                    ref={titleRef}
+                    rotation={[0, 3.164, 0]}
+                >
+                    Mes Projets
+                </Title>
+            </Float>
 
             <CardContainer reducer={reducer} SETTINGS={SETTINGS} />
         </group>
