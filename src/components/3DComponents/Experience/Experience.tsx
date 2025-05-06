13b7c4dd242518290723ce4e88b3df9ffa7db5a5
Adrien <adrienquijo@gmail.com>
2025-05-06 22:26:57 +0200

    code cleanup

--- src/components/3DComponents/Experience/Experience.tsx
+++ src/components/3DComponents/Experience/Experience.tsx
@@ -1,44 +1,10 @@
-import { onControlStart } from '@/components/3DComponents/Scene/Scene.tsx';
 import { SimpleEnvironment } from '@/components/Loaders/Loader.tsx';
-import { CameraControls, Environment } from '@react-three/drei';
+import { Environment } from '@react-three/drei';
 import { Suspense } from 'react';
-
-let minAngle = -Infinity;
-let maxAngle = Infinity;
 
 export function Experience() {
     return (
         <>
-            {/* <CameraControls
-                // makeDefault
-                // no Y-axis
-                polarRotateSpeed={0}
-                // no zoom
-                dollySpeed={0}
-                // Max angle on active is given by the camera
-                maxAzimuthAngle={maxAngle}
-                // Min angle on active is given by the camera
-                minAzimuthAngle={minAngle}
-                ref={ref}
-                mouseButtons={{
-                    // Activate left click for rotation
-                    left: 1,
-                    middle: 0,
-                    right: 0,
-                    // !! IMPORTANT !! DISABLE SCROLL WHEEL for camera
-                    // to activate the scroll on the page
-                    wheel: 0,
-                }}
-                touches={{
-                    // Allows 1 finger touch for rotation
-                    one: 1,
-                    two: 0,
-                    three: 0,
-                }}
-                // onStart={(e) =>
-                //     onControlStart(e, reducer.activeContent?.isClicked)
-                // }
-            /> */}
             <color attach="background" args={['#191920']} />
             <ambientLight intensity={0.1} />
             <pointLight position={[10, 10, 10]} intensity={1} />
