961ed806d88769bb6948f51a2a15d00c476791f6
Adrien <adrienquijo@gmail.com>
2025-05-08 20:08:00 +0200

    Fix du home title qui n'était pas bien positionné lors d'un redimensionnement dynamique -

--- src/components/3DComponents/Title/HomePageTitle.tsx
+++ src/components/3DComponents/Title/HomePageTitle.tsx
@@ -6,17 +6,47 @@ import { Group } from 'three';
  * Main title of the home page.
  *
  * @param ref - Ref of the group
+ * @param scale - Scale of the title from the reducer generalScaleX
  */
-export function HomePageTitle({ ref }: { ref: RefObject<Group> }) {
+export function HomePageTitle({
+    ref,
+    scale,
+}: {
+    ref: RefObject<Group>;
+    scale: number;
+}) {
     return (
         <group ref={ref}>
-            <FloatingTitle scale={1} size={80} yPosition={1}>
+            <FloatingTitle
+                scale={scale}
+                size={80}
+                position-y={1}
+                textProps={{
+                    height: 40,
+                }}
+            >
                 Bienvenue
             </FloatingTitle>
-            <FloatingTitle scale={1} size={60} yPosition={0} back>
+            <FloatingTitle
+                scale={scale}
+                size={60}
+                position-y={0}
+                back
+                textProps={{
+                    height: 40,
+                }}
+            >
                 sur mon
             </FloatingTitle>
-            <FloatingTitle scale={1} size={80} yPosition={-1} back>
+            <FloatingTitle
+                scale={scale}
+                size={80}
+                position-y={-1}
+                back
+                textProps={{
+                    height: 40,
+                }}
+            >
                 Portfolio !
             </FloatingTitle>
         </group>
