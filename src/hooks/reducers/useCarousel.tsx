817d920a528fe20b24b46967b81a5ef66932bf67
Adrien <adrienquijo@gmail.com>
2025-05-08 11:33:41 +0200

    Ajout d'un batchupdate pour les propriétés du states - Ajout de callbacks permettant de mettre à jour individuellement ces propriétés - Ces propriété sont maintenant des propriété du state avec des valeur initiales -

--- src/hooks/reducers/useCarousel.tsx
+++ src/hooks/reducers/useCarousel.tsx
@@ -6,6 +6,18 @@ import { carouselReducer } from '@/hooks/reducers/carouselReducer.tsx';
 const initialState: CarouselState = {
     elements: [],
     loadedCount: 0,
+    isMobile: false,
+    isTablet: false,
+    contentSizes: null,
+    contentWidth: null,
+    contentHeight: null,
+    generalScaleX: 1,
+    // generalScales: {
+    //     generalScaleX: 1,
+    //     generalScaleY: 1,
+    // },
+    generalScaleY: 1,
+    visible: 'home',
 };
 
 /**
@@ -13,11 +25,8 @@ const initialState: CarouselState = {
  */
 export function useCarousel(): ReducerType {
     const [state, dispatch] = useReducer(carouselReducer, initialState);
+
     let activeContent = null;
-    let contentSizes = null;
-    let contentWidth = null;
-    let contentHeight = null;
-    let visible = null;
 
     if (state.elements.length > 0) {
         activeContent = state.elements.find(
@@ -26,16 +35,20 @@ export function useCarousel(): ReducerType {
     }
     return {
         loadedCount: state.loadedCount,
-        visible: visible,
-        isMobile: false,
-        isTablet: false,
-        contentSizes: contentSizes,
-        contentWidth: contentWidth,
-        contentHeight: contentHeight,
-        generalScaleX: 1,
-        generalScaleY: 1,
+        visible: state.visible,
+        isMobile: state.isMobile,
+        isTablet: state.isTablet,
+        contentSizes: state.contentSizes,
+        contentWidth: state.contentWidth,
+        contentHeight: state.contentHeight,
+        generalScaleX: state.generalScaleX,
+        // generalScales: state.generalScales,
+        generalScaleY: state.generalScaleY,
         activeContent: activeContent,
         showElements: state.elements,
+        batchUpdate: useCallback((updates) => {
+            dispatch({ type: 'BATCH_UPDATE', payload: updates });
+        }, []),
         updateElements: useCallback((element) => {
             if (!element || !element.id) {
                 console.warn('Element invalide pour mise à jour', element);
@@ -101,6 +114,83 @@ export function useCarousel(): ReducerType {
                 dispatch({
                     type: 'UPDATE_LOAD_COUNT',
                     payload: increment,
+                }),
+            []
+        ),
+        // resetLoadCount: useCallback(
+        //     () => dispatch({ type: 'RESET_LOAD_COUNT' }),
+        //     []
+        // ),
+        setContentSizes: useCallback(
+            (sizes) =>
+                dispatch({
+                    type: 'SET_CONTENT_SIZES',
+                    payload: sizes,
+                }),
+            []
+        ),
+        setContentWidth: useCallback(
+            (width) =>
+                dispatch({
+                    type: 'SET_CONTENT_WIDTH',
+                    payload: width,
+                }),
+
+            []
+        ),
+        setContentHeight: useCallback(
+            (height) =>
+                dispatch({
+                    type: 'SET_CONTENT_HEIGHT',
+                    payload: height,
+                }),
+            []
+        ),
+        setGeneralScaleX: useCallback(
+            (scale) =>
+                dispatch({
+                    type: 'SET_GENERAL_SCALEX',
+                    payload: scale,
+                }),
+            []
+        ),
+        setGeneralScaleY: useCallback(
+            (scale) =>
+                dispatch({
+                    type: 'SET_GENERAL_SCALEY',
+                    payload: scale,
+                }),
+            []
+        ),
+        // setGeneralScales: useCallback(
+        //     (scales) =>
+        //         dispatch({
+        //             type: 'SET_GENERAL_SCALES',
+        //             payload: scales,
+        //         }),
+        //     []
+        // ),
+        setViewMode: useCallback(
+            (viewMode) =>
+                dispatch({
+                    type: 'SET_VIEW_MODE',
+                    payload: viewMode,
+                }),
+            []
+        ),
+        setTablet: useCallback(
+            (property) =>
+                dispatch({
+                    type: 'SET_TABLET',
+                    payload: property,
+                }),
+            []
+        ),
+        setMobile: useCallback(
+            (property) =>
+                dispatch({
+                    type: 'SET_MOBILE',
+                    payload: property,
                 }),
             []
         ),
