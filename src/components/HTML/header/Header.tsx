af64db955c98206276e37fd63384a47193e04d11
Adrien <adrienquijo@gmail.com>
2025-05-12 23:19:14 +0200

    Suppression d'un state qui était inutile - La nav est maintenant toujours rendu mais modifiée en fonction de la classe appliquée directement - Code Clenaup -

--- src/components/HTML/header/Header.tsx
+++ src/components/HTML/header/Header.tsx
@@ -1,72 +1,77 @@
 import { Button } from '@/components/HTML/button/Button';
 import { NavLink } from 'react-router';
 import '@css/NavHeader.scss';
-import { MouseEvent, RefObject, useEffect, useRef, useState } from 'react';
+import { MouseEvent, useEffect, useRef, useState } from 'react';
 import { useTouchEvents } from '@/hooks/Touch/useTouchEvents';
 
 export function Header({ isTouchDevice }) {
-    const headerRef = useRef<RefObject<HTMLElement>>(null!);
-    const buttonRef = useRef<RefObject<HTMLButtonElement>>(null!);
-    const [isClicked, setClicked] = useState(false);
-    const [isMoving, setIsMoving] = useState(false);
+    const headerRef = useRef<HTMLElement>(null!);
+    const buttonRef = useRef<HTMLButtonElement>(null!);
+    const [isOpen, setIsOpen] = useState(false);
 
-    const click = useTouchEvents(buttonRef, headerRef);
-    const active = isMoving || isClicked ? 'active' : 'inactive';
+    const { isMoving, setDrawerState } = useTouchEvents(buttonRef, headerRef, {
+        onStateChange: (newIsOpen) => setIsOpen(newIsOpen),
+    });
+    const active = isOpen ? 'active' : 'inactive';
 
     const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
-        setClicked(!isClicked);
-        setIsMoving(!isClicked);
+        setIsOpen(!isOpen);
     };
 
     const handleMouseOver = (e: MouseEvent<HTMLElement>) => {
         e.preventDefault();
-        setIsMoving(!isMoving);
-        setClicked(!isMoving);
+        setIsOpen(!isOpen);
+    };
+
+    const handleMouseOut = (e: MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        if (!isOpen) return;
+        setIsOpen(!isOpen);
     };
 
     useEffect(() => {
         headerRef.current?.removeAttribute('style');
-    }, [isMoving, isClicked]);
+    }, [isOpen]);
 
     return (
         <header
             ref={headerRef}
             className={`main-container ${active}`}
             onMouseEnter={handleMouseOver}
-            onMouseLeave={handleMouseOver}
+            onMouseLeave={handleMouseOut}
         >
             <Button ref={buttonRef} type="button" onClick={handleClick}>
                 Open
             </Button>
-            {isMoving && (
-                <nav>
-                    {/* <Logo /> */}
-                    <ul>
-                        <li>
-                            <NavLink
-                                id="home-link"
-                                aria-labelledby="home-link-text"
-                                to="/"
-                            >
-                                <span id="home-link-text">Accueil</span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/a-propos">A Propos</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/projets">Mes Projets</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/contact">Me Contacter</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/more">More</NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            )}
+            {/* {isOpen && ( */}
+            <nav className={isOpen ? 'active' : 'inactive'}>
+                {/* <Logo /> */}
+                <ul>
+                    <li>
+                        <NavLink
+                            id="home-link"
+                            aria-labelledby="home-link-text"
+                            to="/"
+                        >
+                            <span id="home-link-text">Accueil</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/a-propos">A Propos</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/projets">Mes Projets</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contact">Me Contacter</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/more">More</NavLink>
+                    </li>
+                </ul>
+            </nav>
+            {/* )} */}
         </header>
     );
 }
