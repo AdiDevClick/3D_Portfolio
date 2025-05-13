57b87682536ef23df9ad5f1b2303ced96fedd4d9
Adrien <adrienquijo@gmail.com>
2025-05-13 21:03:46 +0200

    Ajout de types correspondant au hook

--- src/hooks/Touch/useTouchEventsTypes.ts
+++ src/hooks/Touch/useTouchEventsTypes.ts
@@ -1,24 +1,27 @@
 import { RefObject } from 'react';
 
-export interface Origin {
-    x: number;
-    y: number;
-}
+export type Origin = { x: number; y: number } | null;
 
 export interface TouchEventProps {
     /** Element that will receive the transition */
     transitionElement: RefObject<HTMLElement>;
-    setClicked: (clicked: boolean) => void;
+    setIsClickInProgress: (isClickInProgress: boolean) => void;
     // setClicked: Dispatch<SetStateAction<boolean>>;
-    isClick: boolean;
+    isClickInProgress: boolean;
     setOrigin: (origin: Origin) => void;
-    // setOrigin: (origin: { x?: number; y?: number }) => void;
-    // setOrigin: Dispatch<SetStateAction<{ x?: number; y?: number }>>;
     /** The origin of the transition using X & Y */
-    origin: Origin | null;
+    origin: Origin;
     // origin: { x?: number; y?: number };
     setIsMoving: (isMoving: boolean) => void;
     // setIsMoving: Dispatch<SetStateAction<boolean>>;
     isMoving: boolean;
     activeElementRef: RefObject<HTMLElement>;
+    lastTranslate: { x: number; y: number } | null;
+    setLastTranslate: (lastTranslate: { x: number; y: number } | null) => void;
+    elementSize: { width: number; height: number } | null;
+    setElementSize: (
+        elementSize: { width: number; height: number } | null
+    ) => void;
+    abortControllerRef: RefObject<AbortController | null>;
+    setIsOpen: (isOpen: boolean) => void;
 }
