ad7a52eb81077e969ace7bfe124969acbe57db52
Adrien <adrienquijo@gmail.com>
2025-05-14 19:19:08 +0200

    Ajout de <Sparkles /> proche du titre principal -

--- src/components/3DComponents/Title/HomePageTitle.tsx
+++ src/components/3DComponents/Title/HomePageTitle.tsx
@@ -1,4 +1,5 @@
 import FloatingTitle from '@/components/3DComponents/Title/FloatingTitle.tsx';
+import { Sparkles } from '@react-three/drei';
 import { RefObject } from 'react';
 import { Group } from 'three';
 
@@ -25,6 +26,7 @@ export function HomePageTitle({
                     height: 40,
                 }}
             >
+                <Sparkles count={10} size={6} speed={0.4} color={'blue'} />
                 Bienvenue
             </FloatingTitle>
 
@@ -50,6 +52,63 @@ export function HomePageTitle({
             >
                 Portfolio !
             </FloatingTitle>
+            {/* <Glow
+                scale={size * 1.2}
+                near={-25}
+                color={glow || emissive || color}
+            /> */}
         </group>
     );
 }
+// const Glow = ({ color, scale = 0.5, near = -2, far = 1.4 }) => (
+//     <Billboard>
+//         <mesh>
+//             <circleGeometry args={[2 * scale, 16]} />
+//             <LayerMaterial
+//                 transparent
+//                 depthWrite={false}
+//                 blending={THREE.CustomBlending}
+//                 blendEquation={THREE.AddEquation}
+//                 blendSrc={THREE.SrcAlphaFactor}
+//                 blendDst={THREE.DstAlphaFactor}
+//             >
+//                 <Depth
+//                     colorA={color}
+//                     colorB="black"
+//                     alpha={1}
+//                     mode="normal"
+//                     near={near * scale}
+//                     far={far * scale}
+//                     origin={[0, 0, 0]}
+//                 />
+//                 <Depth
+//                     colorA={color}
+//                     colorB="black"
+//                     alpha={0.5}
+//                     mode="add"
+//                     near={-40 * scale}
+//                     far={far * 1.2 * scale}
+//                     origin={[0, 0, 0]}
+//                 />
+//                 <Depth
+//                     colorA={color}
+//                     colorB="black"
+//                     alpha={1}
+//                     mode="add"
+//                     near={-15 * scale}
+//                     far={far * 0.7 * scale}
+//                     origin={[0, 0, 0]}
+//                 />
+//                 <Depth
+//                     colorA={color}
+//                     colorB="black"
+//                     alpha={1}
+//                     mode="add"
+//                     near={-10 * scale}
+//                     far={far * 0.68 * scale}
+//                     origin={[0, 0, 0]}
+//                 />
+//             </LayerMaterial>
+//         </mesh>
+//     </Billboard>
+// );
