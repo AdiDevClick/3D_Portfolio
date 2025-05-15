60c959d961d59ea477d2eeb5da0517203f8c950b
Adrien <adrienquijo@gmail.com>
2025-05-15 15:40:44 +0200

    autre

--- src/components/TODELETE/Custom3DObject.tsx
+++ src/components/TODELETE/Custom3DObject.tsx
@@ -1,35 +1,37 @@
-import { Bone, BoxGeometry, PlaneGeometry, SphereGeometry } from "three";
+import { Bone, BoxGeometry, PlaneGeometry, SphereGeometry } from 'three';
 
-
-export function Custom3DObject({type, size, segments,  ...props}) {
-    let ThreeDObject
+export function Custom3DObject({ type, size, segments, ...props }) {
+    let ThreeDObject;
     switch (type) {
-        case "box":
-            ThreeDObject = new BoxGeometry(size.width, size.height, size.depth)
+        case 'box':
+            ThreeDObject = new BoxGeometry(size.width, size.height, size.depth);
             break;
-        case "plane" : 
-        ThreeDObject = new PlaneGeometry(size.width, size.height)
-        break;
-        case "sphere" : 
-        ThreeDObject = new SphereGeometry(size.width, size.height,  size.depth)
-        break;
+        case 'plane':
+            ThreeDObject = new PlaneGeometry(size.width, size.height);
+            break;
+        case 'sphere':
+            ThreeDObject = new SphereGeometry(
+                size.width,
+                size.height,
+                size.depth
+            );
+            break;
         default:
             break;
     }
-    ThreeDObject.width = size.width
-    ThreeDObject.height = size.height
-    ThreeDObject.depth = size.depth
+    ThreeDObject.width = size.width;
+    ThreeDObject.height = size.height;
+    ThreeDObject.depth = size.depth;
 
-    ThreeDObject.depth = size.depth
-    ThreeDObject.depth = size.depth
-    ThreeDObject.depth = size.depth
+    ThreeDObject.depth = size.depth;
+    ThreeDObject.depth = size.depth;
+    ThreeDObject.depth = size.depth;
 
-
-    const bones = new Bone
-    ThreeDObject
-    return ()
+    const bones = new Bone();
+    ThreeDObject;
+    return;
 }
 
 function inCollision(object1, object2) {
-    return 
+    return;
 }
