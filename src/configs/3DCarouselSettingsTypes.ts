dde1328764a425342410908027cea1acb347c2a2
Adrien <adrienquijo@gmail.com>
2025-05-15 16:55:23 +0200

    ajout de types

---
+++ src/configs/3DCarouselSettingsTypes.ts
@@ -0,0 +1,45 @@
+export interface CarouselGeneralSettings {
+    CONTAINER_SCALE: number;
+    CARD_SCALE: number;
+    ACTIVE_CARD: boolean;
+    CARD_ANIMATION: {
+        value: 'Move_Out' | 'Move_In';
+        options: ('Move_Out' | 'Move_In')[];
+    };
+    CARDS_COUNT: number;
+    CARD_SPEED: number;
+}
+
+export interface BoundariesSettings {
+    attachCamera: boolean;
+    debug: boolean;
+    x: number;
+    y: number;
+    z: number;
+    path: {
+        value: 'Circle' | 'Rollercoaster' | 'Infinity' | 'Heart';
+        options: ('Circle' | 'Rollercoaster' | 'Infinity' | 'Heart')[];
+    };
+}
+
+export interface CardsSettings {
+    THREED: boolean;
+    ALIGNMENT: boolean;
+    BENDING: number;
+    y_HEIGHT: number;
+}
+
+export interface PresenceSettings {
+    PRESENCE_CIRCLE: boolean;
+    COLLISIONS: boolean;
+    PRESENCE_RADIUS: number;
+    CARD_WIREFRAME: boolean;
+}
+
+export interface SettingsType
+    extends CarouselGeneralSettings,
+        BoundariesSettings,
+        CardsSettings,
+        PresenceSettings {
+    set: any;
+}
