c522628ef50437923f46d5830ea21f612544b9b4
Adrien <adrienquijo@gmail.com>
2025-05-15 22:14:14 +0200

    Meilleur typage - Code cleanup

--- src/components/3DComponents/Title/FallbackText.tsx
+++ src/components/3DComponents/Title/FallbackText.tsx
@@ -1,9 +1,11 @@
 import { Text, TextProps } from '@react-three/drei';
 import normalMontFont from '@assets/fonts/Montserrat-VariableFont_wght.ttf';
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
+import { Group, Mesh } from 'three';
 
 type FallbackTextTypes = {
     children: ReactNode;
+    ref?: React.RefObject<Mesh | Group>;
 } & TextProps;
 
 /**
@@ -11,15 +13,14 @@ type FallbackTextTypes = {
  *
  * @returns
  */
-export function FallbackText({ children, ...props }: FallbackTextTypes) {
+export function FallbackText({ children, ref, ...props }: FallbackTextTypes) {
     return (
         <Text
+            ref={ref}
             castShadow
             font={normalMontFont}
             fontSize={0.1}
             color="black"
-            // anchorX="center"
-            // anchorY="middle"
             outlineWidth={0.004}
             {...props}
         >
