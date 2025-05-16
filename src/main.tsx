b9e18b97b3fa87bdf3b95c58a8b73ae641146a7c
Adrien <adrienquijo@gmail.com>
2025-05-16 09:05:04 +0200

    autre

--- src/main.tsx
+++ src/main.tsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter(
                     ),
                 },
                 {
-                    path: 'contact',
+                    path: 'test',
                     element: <Error404 />,
                 },
             ],
