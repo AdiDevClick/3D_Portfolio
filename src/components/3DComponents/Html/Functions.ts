49b4bbb767fe10e99cbf0cdc98f47713577a18ed
Adrien <adrienquijo@gmail.com>
2025-05-17 17:50:48 +0200

    code cleanup

--- src/components/3DComponents/Html/Functions.ts
+++ src/components/3DComponents/Html/Functions.ts
@@ -1,5 +1,3 @@
-import { Vector3 } from 'three';
-
 interface MeasureTypes {
     scaleRatio: number;
     setScaleRatio: (newRatio: number) => void;
@@ -26,45 +24,12 @@ export function measure(
     }
 
     const { width } = element.getBoundingClientRect();
-    // const viewportWidth = element.parentElement.clientWidth;
     const viewportWidth = element.clientWidth;
-    // const viewportWidth = window.innerWidth;
     let newRatio = viewportWidth / width;
 
-    // if (camera && element.parentElement) {
-    //     // Récupérer la position 3D du conteneur parent
-    //     const parentPos = new Vector3();
-    //     element.getWorldPosition(parentPos);
-
-    //     // Calculer la distance réelle à la caméra
-    //     const distanceToCamera = camera.position.distanceTo(parentPos);
-    //     if (distanceToCamera > 0.1) {
-    //         const idealDistance = 10;
-    //         const distanceRatio = idealDistance / distanceToCamera;
-    //         newRatio *= 1 * distanceRatio * 0.1;
-    //         newRatio = Math.max(0.5, Math.min(newRatio, 2));
-    //     }
-    // }
-
     if (width !== viewportWidth && !done && scaleRatio !== newRatio) {
-        console.log(
-            'newRatio',
-            newRatio,
-            'viewport',
-            viewportWidth,
-            'width',
-            width,
-            'element',
-            element
-        );
-
         setDone(true);
         setScaleRatio(newRatio);
-        // setScaleRatio((prev) => (prev < newRatio ? newRatio : prev));
-        // document.documentElement.style.setProperty(
-        //     '--compensation-scale',
-        //     newRatio.toString()
-        // );
     } else {
         setDone(true);
     }
