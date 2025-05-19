36cfe455564510659fb407637e58c1ca3f635f37
Adrien <adrienquijo@gmail.com>
2025-05-19 22:21:11 +0200

    Ajout d'un nouveau font

--- src/configs/3DFonts.config.ts
+++ src/configs/3DFonts.config.ts
@@ -1,3 +1,6 @@
 import montserratFont from '@assets/fonts/Montserrat_Thin_Regular.json';
+import interFont from '@assets/fonts/Inter_Regular.json';
+import helveticFont from '@assets/fonts/helvetiker_regular.typeface.json';
 import { FontData } from '@react-three/drei';
-export const importedFont = montserratFont as unknown as FontData;
+// export const importedFont = montserratFont as unknown as FontData;
+export const importedFont = helveticFont as unknown as FontData;
