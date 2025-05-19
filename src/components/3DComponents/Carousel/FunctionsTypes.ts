557e4b64d80d46853816ac7140ce4e97931cde0f
Adrien <adrienquijo@gmail.com>
2025-05-19 23:48:50 +0200

    exportation de types

--- src/components/3DComponents/Carousel/FunctionsTypes.ts
+++ src/components/3DComponents/Carousel/FunctionsTypes.ts
@@ -3,6 +3,7 @@
  * @author Adrien Quijo
  */
 
+import { SettingsType } from '@/configs/3DCarouselSettingsTypes';
 import { ElementType, ReducerType } from '@/hooks/reducers/carouselTypes.ts';
 import { Vector3 } from 'three';
 
@@ -18,4 +19,10 @@ export interface CollisionConfig {
     margin: number;
     deltaScale: number;
     minScaleDifference: number;
+}
+
+export interface CarouselProps {
+    reducer: ReducerType;
+    boundaries: { x: number; y: number; z: number };
+    SETTINGS: SettingsType;
 }
