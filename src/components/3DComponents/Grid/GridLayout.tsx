961783f3c56dea8c5c019ebdd5eb8bab97fe86a5
Adrien <adrienquijo@gmail.com>
2025-05-19 23:10:00 +0200

    exportation des types

--- src/components/3DComponents/Grid/GridLayout.tsx
+++ src/components/3DComponents/Grid/GridLayout.tsx
@@ -1,30 +1,6 @@
-import { JSX, ReactNode, useCallback, useEffect, useState } from 'react';
+import { GridLayoutProps } from '@/components/3DComponents/Grid/GridLayoutTypes';
+import { useCallback, useEffect, useState } from 'react';
 import { Box3, Vector3, Group } from 'three';
-
-export type GridLayoutProps = {
-    children: ReactNode;
-    /** Width of the grid container/device window size */
-    width: number;
-    /** Index of the current item in the grid */
-    index: number;
-    /** Total number of items in the grid */
-    length: number;
-    /** Scalar value to adjust the size of the grid items and text - @defaultValue 1 */
-    scalar?: number;
-    /** Grid layout options */
-    options?: {
-        /** Number of columns in the grid - @defaultValue 3 */
-        columnsNumber: number;
-        /** Offset for odd rows - @defaultValue 0.5 */
-        rowOffset: number;
-        /** Horizontal margin between items - @defaultValue 2.5 */
-        marginX: number;
-        /** Horizontal margin between items - @defaultValue 1.3 */
-        marginY: number;
-        /** Margin for the window - @defaultValue 1 */
-        windowMargin: number;
-    };
-} & JSX.IntrinsicElements['group'];
 
 /**
  * GridLayout component to arrange items in a grid layout.
