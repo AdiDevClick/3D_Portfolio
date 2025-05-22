11dd473e0be02b54229da42efb6795b4adc80feb
Adrien <adrienquijo@gmail.com>
2025-05-22 18:49:26 +0200

    modification du debouncer

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useRef } from 'react';
 
 type useDebounce = (func: () => void, delay: number) => () => void;
 
@@ -9,24 +9,55 @@ type useDebounce = (func: () => void, delay: number) => () => void;
  * les paramètres de la fonction debounced
  * @fires {debounce}
  */
-export default function useDebounce(func: () => void, delay: number) {
-    const [debouncedFunc, setDebouncedFunc] = useState(func);
-    let context = this;
-    const args = { ...this };
-    let timer;
-    useEffect(() => {
-        new Promise((resolve) => {
-            timer = setTimeout(() => {
-                func.apply(context, args);
-                resolve(delay);
+// export default function useDebounce(func: () => void, delay: number) {
+//     const [debouncedFunc, setDebouncedFunc] = useState(func);
+//     let context = this;
+//     const args = { ...this };
+//     let timer;
+//     useEffect(() => {
+//         new Promise((resolve) => {
+//             timer = setTimeout(() => {
+//                 func.apply(context, args);
+//                 resolve(delay);
+//             }, delay);
+//         });
+//         // const timeoutId = setTimeout(() => {
+//         //     setDebouncedFunc(func);
+//         // }, delay);
+
+//         return () => clearTimeout(timer);
+//     }, [func, delay]);
+
+//     return debouncedFunc;
+// }
+
+/**
+ * Hook qui retourne une version debounced d'une fonction
+ * @param callback Fonction à debouncer
+ * @param delay Délai en millisecondes
+ * @returns Fonction debounced
+ */
+export default function useDebounce<T extends (...args: any[]) => void>(
+    callback: T,
+    delay: number
+): (...args: Parameters<T>) => void {
+    // Utiliser useRef pour stocker le timer
+    const timerRef = useRef<number | null>(null);
+
+    // Utiliser useCallback pour mémoriser la fonction debounced
+    return useCallback(
+        (...args: Parameters<T>) => {
+            // Annuler le timer précédent s'il existe
+            if (timerRef.current) {
+                clearTimeout(timerRef.current);
+            }
+
+            // Créer un nouveau timer
+            timerRef.current = setTimeout(() => {
+                callback(...args);
+                timerRef.current = null;
             }, delay);
-        });
-        // const timeoutId = setTimeout(() => {
-        //     setDebouncedFunc(func);
-        // }, delay);
-
-        return () => clearTimeout(timer);
-    }, [func, delay]);
-
-    return debouncedFunc;
+        },
+        [callback, delay]
+    );
 }
