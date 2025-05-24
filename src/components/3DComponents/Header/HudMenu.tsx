ad7dcb71df85df7eeda66868d75e19b8dc62ecb5
Adrien <adrienquijo@gmail.com>
2025-05-24 03:03:26 +0200

    Meilleur positionnement sur mobile et desktop en prenant en compte la perspective cam

--- src/components/3DComponents/Header/HudMenu.tsx
+++ src/components/3DComponents/Header/HudMenu.tsx
@@ -1,16 +1,22 @@
 import MemoizedIconsContainer from '@/components/3DComponents/3DIcons/IconsContainer';
 import { ReducerType } from '@/hooks/reducers/carouselTypes';
-import { Center, Hud } from '@react-three/drei';
+import { Hud, PerspectiveCamera } from '@react-three/drei';
 import { memo, useRef } from 'react';
 import { Group } from 'three';
 import iconsWithText from '@data/hud-menu.json';
-import { DESKTOP_HTML_ICONS_POSITION_SETTINGS } from '@/configs/3DCarousel.config';
+import {
+    DESKTOP_HUD_ICONS_POSITION_SETTINGS,
+    MOBILE_HUD_ICONS_POSITION_SETTINGS,
+} from '@/configs/3DCarousel.config';
+import { ThreeEvent } from '@react-three/fiber';
+import { handleHudClick } from '@/components/3DComponents/Header/HudFonctions';
+import { useNavigate } from 'react-router';
 
 const floatOptions = {
     autoInvalidate: true,
-    speed: 1.5,
-    rotationIntensity: 0.5,
-    floatIntensity: 0.5,
+    speed: 0.8,
+    rotationIntensity: 0.2,
+    floatIntensity: 0.2,
     floatingRange: [-0.1, 0.1] as [number, number],
 };
 
@@ -22,52 +28,62 @@ const MemoizedHudMenu = memo(function HudMenu({
     /** @defaultValue 0.5 */
     margin?: number;
 }) {
-    const homeButonRef = useRef<Group>(null!);
-    const aboutButtonRef = useRef<Group>(null!);
-    const contactButtonRef = useRef<Group>(null!);
-    const projectsButtonRef = useRef<Group>(null!);
+    const navigate = useNavigate();
 
     const { isMobile, contentWidth, contentHeight, generalScaleX } = reducer;
+
+    const eventsList = {
+        onClick: (event: ThreeEvent<MouseEvent>) => {
+            handleHudClick(event, navigate);
+        },
+    };
 
     const gridOptions = {
         columnsNumber: isMobile ? 4 : 1,
         rowOffset: 0,
-        marginX: 2.5,
-        marginY: 1.5,
-        windowMargin: 1,
+        marginX: isMobile ? 2.5 : 2.5,
+        marginY: isMobile ? 0.5 : 1.5,
+        windowMargin: 0.5,
+        forceColumnsNumber: isMobile ? true : false,
     };
-    const calculatedHeight =
-        -contentHeight + contentHeight - 0.5 * generalScaleX;
-    const iconsPos = DESKTOP_HTML_ICONS_POSITION_SETTINGS(
+    const calculatedHeight = contentHeight - contentHeight * generalScaleX;
+
+    const iconsPos = DESKTOP_HUD_ICONS_POSITION_SETTINGS(
         calculatedHeight,
         contentWidth,
-        0.5
+        { x: -0.5, y: 0.5 }
+    );
+
+    const iconsPosMobile = MOBILE_HUD_ICONS_POSITION_SETTINGS(
+        contentHeight,
+        contentWidth,
+        { x: 0.2, y: 0.25 }
     );
 
     return (
         <Hud>
-            {/* <Center> */}
-            <MemoizedIconsContainer
-                width={contentWidth ?? 1}
-                icons={iconsWithText}
-                scalar={0.4 * generalScaleX}
-                gridOptions={gridOptions}
-                iconScale={10}
-                floatOptions={floatOptions}
-                // position-y={
-                //     isMobile
-                //         ? -2 * generalScaleX - margin
-                //         : 0 * generalScaleX - margin
-                // }
-                // position-x={
-                //     isMobile
-                //         ? -2 * generalScaleX - margin
-                //         : 2 * generalScaleX - margin
-                // }
-                position={iconsPos}
-                isMobile={isMobile}
-            />
-            {/* </Center> */}
+            <PerspectiveCamera makeDefault position={[0, 0, 10]} fov={40} />
+
+            <group
+                scale={[-1, 1, -1]}
+                position={
+                    isMobile
+                        ? [iconsPosMobile.x, iconsPosMobile.y, iconsPosMobile.z]
+                        : [-iconsPos.x, iconsPos.y, iconsPos.z]
+                }
+            >
+                <MemoizedIconsContainer
+                    width={contentWidth ?? 1}
+                    icons={iconsWithText}
+                    scalar={0.4 * generalScaleX}
+                    gridOptions={gridOptions}
+                    iconScale={10}
+                    floatOptions={floatOptions}
+                    isMobile={isMobile}
+                    eventsList={eventsList}
+                    mobileTextProps={{ right: true }}
+                />
+            </group>
         </Hud>
     );
 });
