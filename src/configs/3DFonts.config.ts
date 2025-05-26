b07dedaab1999646c23e3921f0767d25d5961484
Adrien <adrienquijo@gmail.com>
2025-05-26 18:46:08 +0200

    L'importation de monserrat se fera avec le CSS

--- src/configs/3DFonts.config.ts
+++ src/configs/3DFonts.config.ts
@@ -1,7 +1,8 @@
-import montserratFont from '@assets/fonts/Montserrat_Thin_Regular.json';
-import interFont from '@assets/fonts/Inter_Regular.json';
+// import montserratFont from '@assets/fonts/Montserrat_Thin_Regular.json';
+// import interFont from '@assets/fonts/Inter_Regular.json';
 import helveticFont from '@assets/fonts/helvetiker_regular.typeface.json';
-import normalMontFont from '@assets/fonts/Montserrat-VariableFont_wght.ttf';
+// import normalMontFont from '@assets/fonts/montserrat-variablefont_wght-webfont.woff';
+// import normalMontFont from '@assets/fonts/Montserrat-VariableFont_wght.ttf';
 
 import { FontData } from '@react-three/drei';
 
@@ -13,4 +14,7 @@ export const importedFont = helveticFont as unknown as FontData;
 /**
  * Text Fallback font for titles
  */
-export const importedNormalFont = normalMontFont;
+// export const importedNormalFont =
+// '/src/assets/fonts/montserrat-variablefont_wght-webfont.woff';
+export const importedNormalFont = 'Montserrat';
+// export const importedNormalFont = normalMontFont;
