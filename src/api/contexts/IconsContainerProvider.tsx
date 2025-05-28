c61823c2cddec39b97d01ae674fd3e3d399cb0b2
Adrien <adrienquijo@gmail.com>
2025-05-28 11:26:49 +0200

    msie à jour depuis le main

--- src/api/contexts/IconsContainerProvider.tsx
+++ src/api/contexts/IconsContainerProvider.tsx
@@ -1,29 +1,19 @@
 import { IconsContainerContextTypes } from '@/components/3DComponents/3DIcons/IconsContainer';
-import { createContext, ReactNode, Ref } from 'react';
-import { Group } from 'three';
+import { createContext, ReactNode } from 'react';
 
 type IconsContainerProviderProps = {
     children: ReactNode;
     value: IconsContainerContextTypes;
-    ref?: Ref<Group>;
 };
 export const IconsContainerContext =
     createContext<IconsContainerContextTypes | null>(null);
 export function IconsContainerProvider({
     children,
     value,
-    ref,
-    ...props
 }: IconsContainerProviderProps) {
-    if (!value) {
-        throw new Error('IconsContainerProvider requires a contextValue prop');
-    }
-
     return (
         <IconsContainerContext.Provider value={value}>
-            <group name="icon__center-container" ref={ref} {...props}>
-                {children}
-            </group>
+            {children}
         </IconsContainerContext.Provider>
     );
 }
