732c087c3c7aaa294f203e443e777f15577601e4
Adrien <adrienquijo@gmail.com>
2025-05-29 11:56:07 +0200

    Utilisation du dracoConfig file - Cela devrait permettre une meilleure mise en cache

--- src/components/3DComponents/3DIcons/Icons.tsx
+++ src/components/3DComponents/3DIcons/Icons.tsx
@@ -1,21 +1,29 @@
+import { dracoConfig } from '@/api/draco/dracoConfig';
 import { IconMesh } from '@/components/3DComponents/3DIcons/IconMesh';
 import { IconsTypes } from '@/components/3DComponents/Contact/ContactTypes';
-import { useCursor, useGLTF } from '@react-three/drei';
+import { useCursor } from '@react-three/drei';
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three-stdlib';
 
 /**
  * Creates a 3D icon component.
  * - Hover effect and click event to open a link
  * - A tooltip is displayed on hover
- * @param props - Props to be passed to the component. Accepts all group props
  * @param model - Model path to be used
+ * @param hovered - Whether the icon is hovered
+ * @param scale - **@default=100** - Scale of the icon
  */
-export function Icons({ model, hovered }: IconsTypes) {
-    const { nodes } = useGLTF(model);
+export function Icons({ model, hovered, scale = 100 }: IconsTypes) {
+    const { nodes } = useLoader(GLTFLoader, model, (loader) => {
+        const dracoLoader = dracoConfig();
+
+        loader.setDRACOLoader(dracoLoader);
+    });
 
     useCursor(hovered || false);
 
     return (
-        <group dispose={null}>
+        <group>
             {nodes.Scene?.children.map((node) => {
                 return (
                     <IconMesh
@@ -24,6 +32,7 @@ export function Icons({ model, hovered }: IconsTypes) {
                         iconColor={'#000000'}
                         curveSegments={32}
                         hovered={hovered}
+                        scale={scale}
                     />
                 );
             })}
@@ -35,9 +44,9 @@ export function Icons({ model, hovered }: IconsTypes) {
  * Allow to preload the icon models
  * @param modelPaths - Array of model paths to preload
  */
-export function preloadIcons(modelPaths: string[]) {
-    const paths = Array.isArray(modelPaths) ? modelPaths : [modelPaths];
-    paths.forEach((path) => {
-        useGLTF.preload(path);
-    });
-}
+// export function preloadIcons(modelPaths: string[]) {
+//     const paths = Array.isArray(modelPaths) ? modelPaths : [modelPaths];
+//     paths.forEach((path) => {
+//         useGLTF.preload(path);
+//     });
+// }
