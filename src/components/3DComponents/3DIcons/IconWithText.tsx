4d5374bd17c5f5971b1f7bb212c0cc5c6226e571
Adrien <adrienquijo@gmail.com>
2025-05-29 11:49:34 +0200

    Le floating title aura des rotations et float intensity random - Il passera le text en props 'text' - Le children passé au <FloatingTitle /> pourra être un autre élément comme une icône par exemple - Les éléments passés ainsi que le texte seront toujours centrés - Le Title étant un <Center />, il peut être aligné à droite ou à gauche - Le ou les autres éléments passés au <Floating /> peut aussi être aligné inversement au title puisqu'il est centré - Si une position=[x,x,x] est spécifée au <Floating/>, le center sera positionné, ce qui pourra aligner tous les éléments passés en children - La clickablebox prendra maintenant la taille du conteneur auquel elle est attachée pour une meilleure expérience utilisateur -

--- src/components/3DComponents/3DIcons/IconWithText.tsx
+++ src/components/3DComponents/3DIcons/IconWithText.tsx
@@ -1,49 +1,23 @@
-import { IconMesh } from '@/components/3DComponents/3DIcons/IconMesh';
 import { Title } from '@/components/3DComponents/Title/Title';
-import { Center, Float, useCursor } from '@react-three/drei';
-import { useLoader } from '@react-three/fiber';
+import { Center, Float, useCursor, useGLTF } from '@react-three/drei';
 import { Group } from 'three';
-import { JSX, use, useMemo, useRef, useState } from 'react';
-import { DRACOLoader, GLTFLoader } from 'three-stdlib';
+import { JSX, Suspense, use, useMemo, useRef, useState } from 'react';
 import { FallbackText } from '@/components/3DComponents/Title/FallbackText';
 import { useSpring, animated } from '@react-spring/three';
 import { IconsContainerContext } from '@/api/contexts/IconsContainerProvider';
 import { IconsContainerContextTypes } from '@/components/3DComponents/3DIcons/IconsContainer';
-// import { easing } from 'maath';
+import { sharedMatrices } from '@/utils/matrices';
+import { PlaceholderIcon } from '@/components/3DComponents/3DIcons/PlaceHolderIcon';
+import { Icons } from '@/components/3DComponents/3DIcons/Icons';
+import { useFrame, useLoader } from '@react-three/fiber';
+import { FontLoader } from 'three-stdlib';
+import { importedFont } from '@/configs/3DFonts.config';
+import FloatingTitle from '@/components/3DComponents/Title/FloatingTitle';
+
+//TODO: Fix sizing for the bbox of the icon with text
 type IconsTypes = {
     model: string;
     datas: { name: string; text: string };
-    // scalar: number;
-    // index: number;
-    /** @defaultValue 0.5 */
-    // margin?: number;
-    // isMobile: boolean;
-    /** @defaultValue 100 */
-    // iconScale?: number;
-    // eventsList?: {
-    //     onClick?: (event: ThreeEvent<MouseEvent>) => void;
-    //     onPointerOver?: (event: ThreeEvent<MouseEvent>) => void;
-    //     onPointerOut?: (event: ThreeEvent<MouseEvent>) => void;
-    //     onPointerDown?: (event: ThreeEvent<MouseEvent>) => void;
-    //     onPointerUp?: (event: ThreeEvent<MouseEvent>) => void;
-    //     [key: string]:
-    //         | ((event: ThreeEvent<MouseEvent | PointerEvent>) => void)
-    //         | undefined;
-    // };
-    // floatOptions?: {
-    //     speed?: number;
-    //     floatIntensity?: number;
-    //     rotationIntensity?: number;
-    //     floatRange?: [number, number];
-    // };
-    // mobileTextProps?: CenterProps;
-    // animations?: {
-    //     propertiesToCheck?: string[];
-    //     hovered?: boolean;
-    //     [key: string]: any;
-    // } & SpringProps;
-    // /** @defaultValue false */
-    // hovered?: boolean;
 } & JSX.IntrinsicElements['group'];
 
 let isComponentMounted = false;
@@ -75,6 +49,7 @@ export function IconWithText({ model, datas, ...props }: IconsTypes) {
         isMobile,
         iconScale,
         scalar,
+        isClickable,
     } = contextValue as IconsContainerContextTypes;
 
     scalar = 0.8 * scalar;
@@ -100,17 +75,44 @@ export function IconWithText({ model, datas, ...props }: IconsTypes) {
 
     const animationSpring = useSpring(newAnimationObject);
 
-    const { nodes } = useLoader(GLTFLoader, model, (loader) => {
-        const gltfLoader = loader as GLTFLoader;
-        const dracoLoader = new DRACOLoader();
-        dracoLoader.setDecoderPath(
-            'https://www.gstatic.com/draco/versioned/decoders/1.5.6/'
-        );
-        gltfLoader.setDRACOLoader(dracoLoader);
-    });
+    // const { nodes } = useLoader(GLTFLoader, model, (loader) => {
+    //     const gltfLoader = loader as GLTFLoader;
+    //     const dracoLoader = new DRACOLoader();
+    //     dracoLoader.setDecoderPath(
+    //         'https://www.gstatic.com/draco/versioned/decoders/1.5.6/'
+    //     );
+    //     gltfLoader.setDRACOLoader(dracoLoader);
+    // });
 
+    // const [nodes, setNodes] = useState<any>(null);
+
+    // useMemo(() => {
+    //     gltfLoader.load(
+    //         model,
+    //         (gltf) => setNodes(gltf),
+    //         undefined,
+    //         (error) => console.error('Error loading model:', error)
+    //     );
+    // }, [model]);
+
+    // const { nodes } = useGLTF(model);
+    // const { nodes } = useGLTF(model, true);
+    // const { nodes } = useLoader(GLTFLoader, model, (loader) => {
+    //     // const gltfLoader = loader as GLTFLoader;
+    //     const dracoLoader = new DRACOLoader();
+    //     dracoLoader.setDecoderPath(
+    //         'https://www.gstatic.com/draco/versioned/decoders/1.5.6/'
+    //     );
+    //     loader.setDRACOLoader(dracoLoader);
+    // });
+
+    // const font = useLoader(
+    //     FontLoader,
+    //     'src/assets/fonts/helvetiker_regular.typeface.json'
+    // );
     const groupRef = useRef<Group>(null!);
     // const frameCountRef = useRef(0);
+    // console.log('Model loaded:', model);
 
     useCursor(hovered);
 
@@ -128,33 +130,49 @@ export function IconWithText({ model, datas, ...props }: IconsTypes) {
      * Checks if the icon is in the camera's frustum
      * and enables/disables the scaling ease.
      */
-    // useFrame((_, delta) => {
-    //     if (!groupRef.current) return;
-    //     frameCountRef.current += 1;
-    //     if (frameCountRef.current % 4 === 0) {
-    //         // const contentGrid = groupRef.current.parent?.parent;
-    //         if (!animations.animatePosition) return;
-    //         // if (contentGrid?.visible)
-    //         easing.damp3(
-    //             groupRef.current.position,
-    //             isComponentMounted,
-    //             0.3,
-    //             delta
-    //         );
-    //         // easing.damp3(
-    //         //     groupRef.current.position,
-    //         //     !isComponentMounted
-    //         //         ? animations.animatePosition.from
-    //         //         : animations.animatePosition.default,
-    //         //     2000,
-    //         //     delta
-    //         // );
-    //     }
-    // });
+    useFrame((_, delta) => {
+        // if (!groupRef.current) return;
+        // frameCountRef.current += 1;
+        // if (frameCountRef.current % 4 === 0) {
+        //     // const contentGrid = groupRef.current.parent?.parent;
+        //     if (!animations.animatePosition) return;
+        //     // if (contentGrid?.visible)
+        //     easing.damp3(
+        //         groupRef.current.position,
+        //         isComponentMounted,
+        //         0.3,
+        //         delta
+        //     );
+        //     // easing.damp3(
+        //     //     groupRef.current.position,
+        //     //     !isComponentMounted
+        //     //         ? animations.animatePosition.from
+        //     //         : animations.animatePosition.default,
+        //     //     2000,
+        //     //     delta
+        //     // );
+        // }
+    });
+    // console.log('je rerender iConWithText');
 
     return (
-        <animated.group {...animationSpring}>
-            <Center
+        <animated.group
+            name={datas.name}
+            ref={groupRef as any}
+            onPointerOver={(e) => {
+                e.stopPropagation();
+                set(true);
+            }}
+            onPointerOut={(e) => {
+                e.stopPropagation();
+                set(false);
+            }}
+            {...eventsList}
+            {...props}
+            {...animationSpring}
+        >
+            {/* <group
+                name={datas.name}
                 ref={groupRef as any}
                 onPointerOver={(e) => {
                     e.stopPropagation();
@@ -166,35 +184,64 @@ export function IconWithText({ model, datas, ...props }: IconsTypes) {
                 }}
                 {...eventsList}
                 {...props}
-                name={datas.name}
-                dispose={null}
+                // position-x={isClickable ? -0.5 * scalar : 0}
+            > */}
+            <FloatingTitle
+                scalar={scalar}
+                isMobile={isMobile}
+                size={30}
+                isClickable={true}
+                floatOptions={floatOptions}
+                textProps={{
+                    scale: 0.01 * scalar,
+                }}
+                name={'icons-' + datas.name + '__title'}
+                right
+                position-x={0.2 * scalar}
+                text={datas.text}
             >
-                {/* <Suspense fallback={<PlaceholderIcon />}> */}
-                <Float {...floatOptions}>
-                    <Center
+                <Center left>
+                    <Icons
+                        model={model}
+                        hovered={hovered}
+                        scale={iconScale * scalar}
+                    />
+                </Center>
+                {/* <Title
+                        right
+                        isMobile={isMobile}
+                        name="icons-Container__title"
+                        position-x={0.2 * scalar}
+                        // position-y={-0.1}
+                        size={30}
+                        // font={importedFont}
+                        // font={font.data}
+                        textProps={{ scale: 0.01 * scalar }}
+                        scalar={scalar}
+                        text={datas.text}
+                    /> */}
+            </FloatingTitle>
+            {/* <Suspense fallback={<PlaceholderIcon />}> */}
+            {/* <Float {...floatOptions}> */}
+            {/* <Center
                         position-y={isMobile ? -0.8 * scalar : 0}
                         position-x={isMobile ? 0.1 * scalar : 0}
                         back
                         left
                         bottom={isMobile ? true : false}
-                    >
-                        {nodes.Scene?.children.map((node) => {
-                            return (
-                                <IconMesh
-                                    name="icons-Container__icon"
-                                    key={node.uuid}
-                                    data={node}
-                                    // iconColor={'#000000'}
-                                    // curveSegments={isMobile ? 4 : 32}
-                                    hovered={hovered}
-                                    scale={iconScale * scalar}
-                                    castShadow
-                                    receiveShadow
-                                />
-                            );
-                        })}
-                    </Center>
-                    {!isMobile ? (
+                    > */}
+            {/* <Suspense fallback={<PlaceholderIcon />}> */}
+            {/* <Center left>
+                        <Icons
+                            model={model}
+                            hovered={hovered}
+                            scale={iconScale * scalar}
+                        />
+                    </Center> */}
+
+            {/* </Suspense> */}
+            {/* </Center> */}
+            {/* {!isMobile ? (
                         <Title
                             right
                             isMobile={isMobile}
@@ -214,19 +261,35 @@ export function IconWithText({ model, datas, ...props }: IconsTypes) {
                         >
                             <FallbackText>{datas.text}</FallbackText>
                         </Center>
-                    )}
-
-                    <mesh
+                    )} */}
+            {/* <Suspense fallback={<PlaceholderIcon />}> */}
+            {/* <Suspense fallback={null}> */}
+            {/* <Title
+                        right
+                        isMobile={isMobile}
+                        name="icons-Container__title"
+                        position-x={0.2 * scalar}
+                        // position-y={-0.1}
+                        size={30}
+                        // font={importedFont}
+                        // font={font.data}
+                        textProps={{ scale: 0.01 * scalar }}
+                        scalar={scalar}
+                        text={datas.text}
+                    /> */}
+            {/* </Center> */}
+            {/* </Suspense> */}
+            {/* <mesh
+                        name="clickable-box"
                         scale={isMobile ? 0.7 * scalar : 1.2 * scalar}
-                        position-y={isMobile ? -0.8 * scalar : 0}
-                        position-x={isMobile ? -0.5 * scalar : 0.2 * scalar}
-                        visible={false}
-                    >
-                        <boxGeometry />
-                    </mesh>
-                </Float>
-                {/* </Suspense> */}
-            </Center>
+                        // position-y={isMobile ? -0.8 * scalar : 0}
+                        // position-x={isMobile ? -0.5 * scalar : 0.2 * scalar}
+                        geometry={sharedMatrices.boxGeometry}
+                        visible={true}
+                    /> */}
+            {/* </Float> */}
+            {/* </Suspense> */}
+            {/* </group> */}
         </animated.group>
     );
 }
