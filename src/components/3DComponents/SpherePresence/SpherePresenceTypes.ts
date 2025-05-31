266bb97ce761a55bbbf171e6bda93fea8694602f
Adrien <adrienquijo@gmail.com>
2025-05-31 22:05:57 +0200

    Suppression de la couleur dans le type pour la presenceSphere

--- src/components/3DComponents/SpherePresence/SpherePresenceTypes.ts
+++ src/components/3DComponents/SpherePresence/SpherePresenceTypes.ts
@@ -2,7 +2,6 @@ import { JSX } from 'react';
 export type SpherePresenceTypes = {
     // position: [number, number, number];
     radius: [number, number];
-    color: string;
     /** @defaultValue true */
     visible?: boolean;
 } & JSX.IntrinsicElements['mesh'];
