28f67220f2680865ce0c2c3e176b4f0604f5b976
Adrien <adrienquijo@gmail.com>
2025-06-09 22:25:40 +0200

    code cleanup

--- src/utils/redirectURL.ts
+++ src/utils/redirectURL.ts
@@ -1,17 +1,10 @@
 async function redirect() {
-    const redirect = sessionStorage.redirect;
-    delete sessionStorage.redirect;
-
-    // Si on a un paramètre p dans l'URL (venant de 404.html)
+    // Si on a un paramètre spa-route dans l'URL (venant de 404.html)
     const searchParams = new URLSearchParams(window.location.search);
-    const redirectPath = searchParams.get('p');
+    const redirectPath = searchParams.get('spa-route');
 
     if (redirectPath) {
-        // Effacer le paramètre p de l'URL visible
         history.replaceState(null, '', window.location.pathname + redirectPath);
-    } else if (redirect && redirect !== window.location.href) {
-        // Support pour une ancienne méthode de redirection si présente
-        history.replaceState(null, '', redirect);
     }
 }
 
