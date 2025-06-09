aa4c9b2f9433c4af7a41afec265748f06489a7bd
Adrien <adrienquijo@gmail.com>
2025-06-09 17:56:25 +0200

    cdoe cleanup

--- src/components/3DComponents/Carousel/Carousel.tsx
+++ src/components/3DComponents/Carousel/Carousel.tsx
@@ -206,39 +206,6 @@ export default function Carousel({
     useFrame((state, delta) => {
         if (!projectsRef.current) return;
 
-        // if (activeURL && count <= 0) {
-        //     scroll.offset = 0;
-        //     scroll.delta = 0;
-        //     scroll.el.scrollTo({ top: 0, behavior: 'instant' });
-        //     // state.camera.position.set(
-        //     //     2.4492935982947065e-15, // ≈ 0
-        //     //     1.2246467991473533e-15, // ≈ 0
-        //     //     -20
-        //     // );
-        //     // ✅ AJOUTER : Reset caméra position
-        //     // state.camera.position.set(0, 0, -20);
-
-        //     // ✅ AJOUTER : Reset controls si présents
-        //     // if (state.controls) {
-        //     //     state.controls.target.set(0, 0, 0);
-        //     //     state.controls.update();
-        //     // }
-        //     count++;
-        //     // projectsRef.current.position.y = 0;
-        //     // projectsRef.current.updateMatrixWorld(true);
-        //     console.log('❌ Scroll reset conditions not met:', {
-        //         scroll: !!scroll,
-        //         scrollEl: !!scroll?.el,
-        //         pagesRef: projectsRef.current,
-        //         camera: state.camera,
-        //         control: state.controls,
-        //     });
-        //     if (state.controls) console.log('CONTROLS OK');
-        // }
-
-        // if (!activeURL && count > 0) {
-        //     count = 0;
-        // }
         frameCountRef += 1;
 
         frustumChecker(
