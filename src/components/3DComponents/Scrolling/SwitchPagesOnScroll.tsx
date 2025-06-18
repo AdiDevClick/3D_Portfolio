bce19ee317c92e9371855c78b57a260120fba8c3
Adrien <adrienquijo@gmail.com>
2025-06-18 12:18:54 +0200

    Le switch peut maintenant naviguer vers une page supérieure ou inférieure - La navigation par tactile n'est pas encore active - Ajuster le modulo du useFrame pour augmenter le smoothness de la détection -

--- src/components/3DComponents/Scrolling/SwitchPagesOnScroll.tsx
+++ src/components/3DComponents/Scrolling/SwitchPagesOnScroll.tsx
@@ -1,52 +1,119 @@
+import { ScrollResetProps } from '@/components/3DComponents/Scrolling/scrollTypes';
+import { wait } from '@/functions/promises';
 import { useScroll } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router';
 
+/**
+ * This component listens for scroll events and switches pages based on the scroll position.
+ *
+ * @param visible - The currently visible section of the application
+ */
 export function SwitchPagesOnScroll({ visible }: ScrollResetProps) {
-    const [isScrollCompleted, setIsScrollCompleted] = useState(false);
-    const [isWheeling, setIsWheeling] = useState(false);
+    const [isScrollCompleted, setIsScrollCompleted] = useState<{
+        position?: string;
+        complete?: boolean;
+    }>({});
+    const [isWheeling, setIsWheeling] = useState<{
+        direction?: string;
+        moving?: boolean;
+    }>({});
     const scroll = useScroll();
     const navigate = useNavigate();
 
-    const wheelTimeoutRef = useRef(0);
     const frameCountRef = useRef(0);
 
+    /**
+     * Handles the scroll completion state and navigates to the appropriate page
+     * based on the current visible section and scroll position.
+     */
     useEffect(() => {
-        if (isScrollCompleted) {
+        if (isScrollCompleted.complete) {
+            const reachedBottom =
+                isScrollCompleted.position === 'bottom' ? true : false;
+            const reachedTop =
+                isScrollCompleted.position === 'top' ? true : false;
             switch (visible) {
                 case 'home':
-                    navigate('/a-propos');
-                    setIsScrollCompleted(false);
+                    if (reachedBottom) {
+                        navigate('/a-propos');
+                    } else if (reachedTop) {
+                        navigate('/contact');
+                    }
                     break;
                 case 'about':
-                    navigate('/projets');
-                    setIsScrollCompleted(false);
+                    if (reachedBottom) {
+                        navigate('/projets');
+                    } else if (reachedTop) {
+                        navigate('/');
+                    }
                     break;
                 case 'carousel':
-                    navigate('/contact');
-                    setIsScrollCompleted(false);
+                    if (reachedBottom) {
+                        navigate('/contact');
+                    } else if (reachedTop) {
+                        navigate('/a-propos');
+                    }
                     break;
                 case 'contact':
-                    navigate('/');
-                    setIsScrollCompleted(false);
+                    if (reachedBottom) {
+                        navigate('/');
+                    } else if (reachedTop) {
+                        navigate('/projets');
+                    }
                     break;
                 default:
                     break;
             }
+
+            setIsScrollCompleted(() => ({
+                position: undefined,
+                complete: false,
+            }));
         }
     }, [visible, isScrollCompleted]);
 
+    /**
+     * Handles the wheel event to detect scroll direction
+     */
     useEffect(() => {
-        const handleWheel = () => {
-            setIsWheeling(true);
-            console.log('wheel event detected');
-            clearTimeout(wheelTimeoutRef.current);
-            wheelTimeoutRef.current = setTimeout(() => {
-                console.log('wheel timeout off');
-                setIsWheeling(false);
-                setIsScrollCompleted(false);
-            }, 100);
+        const handleWheel = async (e: WheelEvent) => {
+            e.stopPropagation();
+            const { scrollTop, scrollHeight, clientHeight } =
+                e.currentTarget as HTMLElement;
+
+            let direction = 'none';
+
+            if (scrollTop + e.deltaY < 0 && e.deltaY < 0) {
+                direction = 'up';
+            }
+
+            if (
+                scrollHeight - (scrollTop + e.deltaY) < clientHeight &&
+                e.deltaY > 0
+            ) {
+                direction = 'down';
+            }
+
+            setIsWheeling((prev) => ({
+                ...prev,
+                direction: direction,
+                moving: true,
+            }));
+
+            if (direction !== 'none') {
+                await wait(100);
+
+                setIsWheeling(() => ({
+                    direction: undefined,
+                    moving: false,
+                }));
+                setIsScrollCompleted(() => ({
+                    position: undefined,
+                    complete: false,
+                }));
+            }
         };
 
         const scrollElement = scroll.el;
@@ -54,33 +121,43 @@ export function SwitchPagesOnScroll({ visible }: ScrollResetProps) {
             scrollElement.addEventListener('wheel', handleWheel, {
                 passive: true,
             });
-            console.log('wheel event listener added');
             return () => {
-                console.log('wheel event listener deleted');
                 scrollElement.removeEventListener('wheel', handleWheel);
-                clearTimeout(wheelTimeoutRef.current);
             };
         }
     }, [scroll.el]);
 
     /**
-     * Scroll type error is inevitable
-     * @description : This is a workaround to reset the scroll position
+     * Checks whether the scroll position is at the top or bottom of the page
+     * @description Uses the useScroll hook to monitor the scroll position
+     * and updates the isScrollCompleted state accordingly.
+     *
+     * Modify the frame count modulo to adjust the smoothness of the scroll detection.
      */
     useFrame(() => {
         frameCountRef.current += 1;
-        if (frameCountRef.current % 30 === 0) {
-            if (scroll.offset >= 1 && !isScrollCompleted) {
-                setIsScrollCompleted(true);
-                console.log(scroll);
-            }
-
+        if (frameCountRef.current % 10 === 0 && !isScrollCompleted.complete) {
             if (
-                (visible === 'carousel' || visible === 'contact') &&
-                isWheeling &&
-                !isScrollCompleted
+                (scroll.offset > 0.99 || scroll.pages === 0) &&
+                isWheeling.moving &&
+                isWheeling.direction === 'down'
             ) {
-                setIsScrollCompleted(true);
+                setIsScrollCompleted((prev) => ({
+                    ...prev,
+                    position: 'bottom',
+                    complete: true,
+                }));
+            }
+            if (
+                (scroll.offset < 0.01 || scroll.pages === 0) &&
+                isWheeling.moving &&
+                isWheeling.direction === 'up'
+            ) {
+                setIsScrollCompleted((prev) => ({
+                    ...prev,
+                    position: 'top',
+                    complete: true,
+                }));
             }
         }
     });
