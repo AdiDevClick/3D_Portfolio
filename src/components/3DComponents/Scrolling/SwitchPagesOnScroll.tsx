db603b7df31002d29e967a83b712b5c9ed7325b3
Adrien <adrienquijo@gmail.com>
2025-06-19 16:26:27 +0200

    Le switcher peut maintenant prendre en compte le touch screen - Ajout de types - Refactorisation et optimisation des events pour éviter les closures - Ajout d'un abortController pour démonter les events -

--- src/components/3DComponents/Scrolling/SwitchPagesOnScroll.tsx
+++ src/components/3DComponents/Scrolling/SwitchPagesOnScroll.tsx
@@ -1,9 +1,20 @@
-import { ScrollResetProps } from '@/components/3DComponents/Scrolling/scrollTypes';
+import {
+    ActivatePageSwitcherProps,
+    HandleMoveProps,
+    HandleTouchStartProps,
+    ScrollCompleteState,
+    ScrollResetProps,
+    SetCompleteScrollStatusProps,
+    WheelingState,
+} from '@/components/3DComponents/Scrolling/scrollTypes';
 import { wait } from '@/functions/promises';
+import useDebounce from '@/hooks/useDebounce';
 import { useScroll } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { useNavigate } from 'react-router';
+
+let startY = 0;
 
 /**
  * This component listens for scroll events and switches pages based on the scroll position.
@@ -11,22 +22,70 @@ import { useNavigate } from 'react-router';
  * @param visible - The currently visible section of the application
  */
 export function SwitchPagesOnScroll({ visible }: ScrollResetProps) {
-    const [isScrollCompleted, setIsScrollCompleted] = useState<{
-        position?: string;
-        complete?: boolean;
-    }>({});
-    const [isWheeling, setIsWheeling] = useState<{
-        direction?: string;
-        moving?: boolean;
-    }>({});
+    const [isScrollCompleted, setIsScrollCompleted] =
+        useState<ScrollCompleteState>({});
+    const [isWheeling, setIsWheeling] = useState<WheelingState>({});
     const scroll = useScroll();
     const navigate = useNavigate();
 
     const frameCountRef = useRef(0);
+    const abortControllerRef = useRef<AbortController>(null!);
+
+    const props = {
+        setIsWheeling,
+        setIsScrollCompleted,
+        scroll,
+        visible,
+    };
+
+    const debouncedHandleWheel = useDebounce(({ e }) => {
+        e.stopPropagation();
+        const scrollElement = scroll.el;
+        const { scrollTop, scrollHeight, clientHeight } =
+            scrollElement as HTMLElement;
+        let direction = 'none';
+
+        if (scrollTop + e.deltaY < 0 && e.deltaY < 0) {
+            direction = 'up';
+        }
+
+        if (
+            scrollHeight - (scrollTop + e.deltaY) < clientHeight &&
+            e.deltaY > 0
+        ) {
+            direction = 'down';
+        }
+
+        activatePageSwitcher({
+            setIsWheeling,
+            setIsScrollCompleted,
+            direction,
+            resetDelay: 50,
+        });
+    }, 100);
+
+    /**
+     * Events list with their handlers and options -
+     */
+    const events = useMemo(
+        () => [
+            { name: 'wheel', eventHandler: debouncedHandleWheel },
+            { name: 'touchstart', eventHandler: handleTouchStart },
+            {
+                name: 'touchmove',
+                eventHandler: handleTouchMove,
+                options: { passive: false },
+            },
+            // { name: 'touchend', eventHandler: endDrag },
+        ],
+        []
+    );
 
     /**
      * Handles the scroll completion state and navigates to the appropriate page
      * based on the current visible section and scroll position.
+     *
+     * This effect will save the current visible section to a ref
      */
     useEffect(() => {
         if (isScrollCompleted.complete) {
@@ -34,6 +93,7 @@ export function SwitchPagesOnScroll({ visible }: ScrollResetProps) {
                 isScrollCompleted.position === 'bottom' ? true : false;
             const reachedTop =
                 isScrollCompleted.position === 'top' ? true : false;
+
             switch (visible) {
                 case 'home':
                     if (reachedBottom) {
@@ -67,6 +127,7 @@ export function SwitchPagesOnScroll({ visible }: ScrollResetProps) {
                     break;
             }
 
+            // visibleRef.current = visible;
             setIsScrollCompleted(() => ({
                 position: undefined,
                 complete: false,
@@ -75,57 +136,35 @@ export function SwitchPagesOnScroll({ visible }: ScrollResetProps) {
     }, [visible, isScrollCompleted]);
 
     /**
-     * Handles the wheel event to detect scroll direction
+     * Creates the different event listeners for the scroll element
+     *
+     * @description This will create an abort controller to manage the event listeners
+     * and clean them up on unmount.
      */
     useEffect(() => {
-        const handleWheel = async (e: WheelEvent) => {
-            e.stopPropagation();
-            const { scrollTop, scrollHeight, clientHeight } =
-                e.currentTarget as HTMLElement;
-
-            let direction = 'none';
-
-            if (scrollTop + e.deltaY < 0 && e.deltaY < 0) {
-                direction = 'up';
-            }
-
-            if (
-                scrollHeight - (scrollTop + e.deltaY) < clientHeight &&
-                e.deltaY > 0
-            ) {
-                direction = 'down';
-            }
-
-            setIsWheeling((prev) => ({
-                ...prev,
-                direction: direction,
-                moving: true,
-            }));
-
-            if (direction !== 'none') {
-                await wait(100);
-
-                setIsWheeling(() => ({
-                    direction: undefined,
-                    moving: false,
-                }));
-                setIsScrollCompleted(() => ({
-                    position: undefined,
-                    complete: false,
-                }));
-            }
-        };
-
+        abortControllerRef.current = new AbortController();
         const scrollElement = scroll.el;
+
         if (scrollElement) {
-            scrollElement.addEventListener('wheel', handleWheel, {
-                passive: true,
+            events.forEach((event) => {
+                scrollElement.addEventListener(
+                    event.name as keyof HTMLElementEventMap,
+                    (e) =>
+                        event.eventHandler({
+                            e,
+                            ...props,
+                        } as HandleMoveProps),
+                    {
+                        ...event.options,
+                        signal: abortControllerRef.current?.signal,
+                    }
+                );
             });
             return () => {
-                scrollElement.removeEventListener('wheel', handleWheel);
+                abortControllerRef.current?.abort();
             };
         }
-    }, [scroll.el]);
+    }, [visible]);
 
     /**
      * Checks whether the scroll position is at the top or bottom of the page
@@ -137,30 +176,152 @@ export function SwitchPagesOnScroll({ visible }: ScrollResetProps) {
     useFrame(() => {
         frameCountRef.current += 1;
         if (frameCountRef.current % 10 === 0 && !isScrollCompleted.complete) {
-            if (
-                (scroll.offset > 0.99 || scroll.pages === 0) &&
-                isWheeling.moving &&
-                isWheeling.direction === 'down'
-            ) {
-                setIsScrollCompleted((prev) => ({
-                    ...prev,
-                    position: 'bottom',
-                    complete: true,
-                }));
-            }
-            if (
-                (scroll.offset < 0.01 || scroll.pages === 0) &&
-                isWheeling.moving &&
-                isWheeling.direction === 'up'
-            ) {
-                setIsScrollCompleted((prev) => ({
-                    ...prev,
-                    position: 'top',
-                    complete: true,
-                }));
-            }
+            setCompleteScrollStatus({
+                isWheeling,
+                condition: scroll.offset > 0.99,
+                scroll,
+                direction: 'down',
+                setIsScrollCompleted,
+                position: 'bottom',
+            });
+
+            setCompleteScrollStatus({
+                isWheeling,
+                condition: scroll.offset < 0.01,
+                scroll,
+                direction: 'up',
+                setIsScrollCompleted,
+                position: 'top',
+            });
         }
     });
 
     return null;
+}
+
+/**
+ * Activates the page switcher based on the scroll direction.
+ * @description This function sets the wheeling state to indicate that scrolling is in progress
+ *
+ * @param setIsWheeling - Function to set the wheeling state
+ * @param setIsScrollCompleted - Function to set the scroll completion state
+ * @param direction - The direction of the scroll ('up', 'down', or 'none
+ */
+async function activatePageSwitcher({
+    setIsWheeling,
+    setIsScrollCompleted,
+    direction,
+    resetDelay = 50,
+}: ActivatePageSwitcherProps) {
+    setIsWheeling((prev) => ({
+        ...prev,
+        direction: direction,
+        moving: true,
+    }));
+
+    if (direction !== 'none') {
+        await wait(resetDelay);
+        setIsWheeling(() => ({
+            direction: undefined,
+            moving: false,
+        }));
+        setIsScrollCompleted(() => ({
+            position: undefined,
+            complete: false,
+        }));
+    }
+}
+
+/**
+ * Handles the touch start event to capture the initial touch position
+ *
+ * @description This will set the global variable startY position
+ * to determine the scroll direction later
+ *
+ * @param e - The touch event object
+ */
+function handleTouchStart({ e }: HandleTouchStartProps) {
+    if (e.targetTouches[0]) {
+        e.stopPropagation();
+
+        const eventPoint = e.targetTouches[0];
+        startY = eventPoint.clientY;
+        // setOrigin({ x: eventPoint.screenX, y: eventPoint.screenY });
+    }
+}
+
+/**
+ * Handles the touch move event to determine the scroll direction
+ * @description This will use the setters to update the wheeling state
+ * After some time, it will reset the wheeling state and scroll completion state
+ *
+ * @param visible - The currently visible section of the application
+ * @param e - The touch event object
+ * @param setIsWheeling - Function to set the wheeling state
+ * @param setIsScrollCompleted - Function to set the scroll completion state
+ */
+function handleTouchMove({
+    e,
+    visible,
+    setIsWheeling,
+    setIsScrollCompleted,
+}: HandleMoveProps): void {
+    if (!e.targetTouches[0] || visible === 'carousel' || visible === 'contact')
+        return;
+
+    let direction = 'none';
+    const { scrollTop, scrollHeight, clientHeight } =
+        e.currentTarget as HTMLElement;
+
+    const currentY = e.targetTouches[0].clientY;
+    const isMovingUp = currentY > startY;
+    const isMovingDown = currentY < startY;
+
+    const isAtTop = scrollTop <= 0;
+    const isAtBottom = scrollTop >= scrollHeight - clientHeight;
+
+    if (isMovingUp && isAtTop) {
+        direction = 'up';
+    } else if (isMovingDown && isAtBottom) {
+        direction = 'down';
+    }
+
+    activatePageSwitcher({
+        setIsWheeling,
+        setIsScrollCompleted,
+        direction,
+        resetDelay: 50,
+    });
+}
+
+/**
+ * Sets the scroll completion status based on the current scroll state
+ *
+ * @description This uses the isWheeling state and setter of the scroll state
+ *
+ * @param isWheeling - The current wheeling state
+ * @param condition - The condition to check if the scroll is complete
+ * @param scroll - The scroll state from the useScroll hook
+ * @param direction - The direction of the scroll ('up' or 'down')
+ * @param setIsScrollCompleted - Function to set the scroll completion state
+ * @param position - The position of the scroll ('top' or 'bottom')
+ */
+function setCompleteScrollStatus({
+    isWheeling,
+    condition,
+    scroll,
+    direction,
+    setIsScrollCompleted,
+    position,
+}: SetCompleteScrollStatusProps) {
+    if (
+        (condition || scroll.pages === 0) &&
+        isWheeling.moving &&
+        isWheeling.direction === direction
+    )
+        setIsScrollCompleted((prev) => ({
+            ...prev,
+            position: position,
+            complete: true,
+        }));
 }
--- src/components/3DComponents/Scrolling/scrollTypes.ts
+++ src/components/3DComponents/Scrolling/scrollTypes.ts
@@ -1,5 +1,55 @@
 import { ReducerType } from '@/hooks/reducers/carouselTypes';
+import { ScrollControlsState } from '@react-three/drei';
+import React from 'react';
 
-export type ScrollResetProps = {
+export interface ScrollResetProps {
     visible: ReducerType['visible'];
+}
+
+export type WheelingState = {
+    direction?: string;
+    moving?: boolean;
 };
+
+export type ScrollCompleteState = {
+    position?: string;
+    complete?: boolean;
+};
+
+export type ActivatePageSwitcherProps = {
+    setIsWheeling: React.Dispatch<React.SetStateAction<WheelingState>>;
+    setIsScrollCompleted: React.Dispatch<
+        React.SetStateAction<ScrollCompleteState>
+    >;
+    direction: string;
+    resetDelay?: number;
+};
+
+export interface HandleMoveProps
+    extends HandleTouchStartProps,
+        ScrollResetProps,
+        Omit<ActivatePageSwitcherProps, 'direction' | 'resetDelay'> {
+    scroll: ScrollControlsState;
+}
+
+export interface SetCompleteScrollStatusProps
+    extends Omit<ActivatePageSwitcherProps, 'setIsWheeling' | 'resetDelay'> {
+    isWheeling: WheelingState;
+    condition: boolean;
+    scroll: ScrollControlsState;
+    position: string;
+}
+
+export interface HandleTouchStartProps {
+    e: TouchEvent &
+        React.TouchEvent<HTMLElement> &
+        WheelEvent &
+        React.WheelEvent<HTMLElement> &
+        Event &
+        MouseEvent &
+        React.MouseEvent<HTMLElement>;
+}
+
+export interface ScrollEventsProps
+    extends HandleTouchStartProps,
+        ScrollResetProps {}
