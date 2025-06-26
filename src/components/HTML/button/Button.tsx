4cf0563fbbd9a89c0cf241826db696bb5dc73141
Adrien <adrienquijo@gmail.com>
2025-06-26 16:15:57 +0200

    Le composant est maintenant générique

--- src/components/HTML/button/Button.tsx
+++ src/components/HTML/button/Button.tsx
@@ -4,17 +4,41 @@ import { ButtonProps } from '@/components/HTML/HTMLtypes';
  * Composant bouton réutilisable -
  * Il renvoie un callback au parent -
  */
-export function Button({ children, ref, ...props }: ButtonProps) {
-    /**
-     * Disable default drag behavior to
-     * avoid conflicts -
-     */
-    // const handleDragStart = (e: DragEvent<HTMLButtonElement>) => {
-    //     e.preventDefault();
-    // };
+// export function Button({ children, ref, ...props }: ButtonProps) {
+//     /**
+//      * Disable default drag behavior to
+//      * avoid conflicts -
+//      */
+//     // const handleDragStart = (e: DragEvent<HTMLButtonElement>) => {
+//     //     e.preventDefault();
+//     // };
 
+//     return (
+//         <button ref={ref} {...props}>
+//             {children}
+//         </button>
+//     );
+// }
+
+/**
+ * Button Component
+ *
+ * @description A simple button component that can be used to trigger actions.
+ *
+ * @params children - The content to be displayed inside the button.
+ * @params className - Additional CSS classes to apply to the button.
+ * @params type - The type of the button (default is 'button').
+ * @params ref - A ref to the button element.
+ */
+export function Button<T>({
+    children,
+    ref,
+    className = 'button',
+    type = 'button',
+    ...props
+}: ButtonProps<T>) {
     return (
-        <button ref={ref} {...props}>
+        <button ref={ref} type={type} className={className} {...props}>
             {children}
         </button>
     );
