dd43f615dfc070ffc10bcbe8fe8d1f5f530c02ac
Adrien <adrienquijo@gmail.com>
2025-06-26 16:17:30 +0200

    Modification du contactContent pour utiliser le useActionState

--- src/pages/Contact/ContactContent.tsx
+++ src/pages/Contact/ContactContent.tsx
@@ -1,14 +1,31 @@
+import { action } from '@/api/submit/action';
+import { Button } from '@/components/HTML/button/Button';
+import { useActionState, useRef } from 'react';
+
 /**
  * Contenu HTML de la page contact
  */
 export function ContactContent({ ...props }) {
+    const [state, formAction, isPending] = useActionState(action, undefined);
+
     return (
-        <div {...props}>
+        <form action={formAction} {...props}>
             <h1 style={{ color: 'black' }}>Me Contacter</h1>
             <p style={{ color: 'black' }}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Quisquam, voluptatibus. Lorem ipsum dolor sit amet{' '}
             </p>
-        </div>
+            <input type="email" name="email" placeholder="Votre adresse mail" />
+            <textarea name="message" placeholder="Votre message" rows={5} />
+            <Button type="submit" className="button">
+                {isPending ? "En cours d'envoi..." : 'Me Contacter'}
+            </Button>
+
+            {state?.message && (
+                <p className={state.success ? 'success' : 'error'}>
+                    {state.message}
+                </p>
+            )}
+        </form>
     );
 }
