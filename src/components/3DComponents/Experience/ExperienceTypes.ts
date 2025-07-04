f7e63ae6aa2d2baa7e7c87fdf8b40999dc362f4c
Adrien <adrienquijo@gmail.com>
2025-07-04 12:46:00 +0200

    Ajout des types pour les cameraPositions

--- src/components/3DComponents/Experience/ExperienceTypes.ts
+++ src/components/3DComponents/Experience/ExperienceTypes.ts
@@ -1,5 +1,8 @@
 import { ReducerType } from '@/hooks/reducers/carouselTypes';
+import { CameraControls } from '@react-three/drei';
+import { RefObject } from 'react';
 import { NavigateFunction } from 'react-router';
+import { Vector3 } from 'three';
 
 export interface ErrorWithCause extends Error {
     cause?: { status: number };
@@ -16,4 +19,24 @@ export type loadCardByURLProps = {
     navigate: NavigateFunction;
     visible: ReducerType['visible'];
     setIsURLLoaded: (value: boolean) => void;
+};
+
+export type introProps = {
+    ref: RefObject<CameraControls | null>;
+    setIsIntro: (value: boolean) => void;
+    cameraPositions: CameraPosition;
+};
+
+type BaseCameraPosition = {
+    position: Vector3;
+    target: Vector3;
+    fov: number;
+};
+
+export type CameraPosition = {
+    home: BaseCameraPosition & {
+        startPosition: [number, number, number];
+    };
+    contact: BaseCameraPosition;
+    carousel: BaseCameraPosition;
 };
