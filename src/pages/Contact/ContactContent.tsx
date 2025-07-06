2a2ecec3dab9fd257da3778d3807d43a796bb2c5
Adrien <adrienquijo@gmail.com>
2025-07-06 18:33:44 +0200

    Ajout d'un layout pôur le contact content - Pour future use - non testé proprement

--- src/pages/Contact/ContactContent.tsx
+++ src/pages/Contact/ContactContent.tsx
@@ -1,22 +1,35 @@
-import { action } from '@/api/submit/action';
+import { formActionHandler } from '@/api/submit/action';
 import { Button } from '@/components/HTML/button/Button';
-import { useActionState, useRef } from 'react';
+import { useActionState } from 'react';
 
 /**
  * Contenu HTML de la page contact
  */
 export function ContactContent({ ...props }) {
-    const [state, formAction, isPending] = useActionState(action, undefined);
+    const [state, formAction, isPending] = useActionState(
+        formActionHandler,
+        undefined
+    );
 
     return (
         <form action={formAction} {...props}>
             <h1 style={{ color: 'black' }}>Me Contacter</h1>
-            <p style={{ color: 'black' }}>
+            <p className="contact-form__body" style={{ color: 'black' }}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Quisquam, voluptatibus. Lorem ipsum dolor sit amet{' '}
             </p>
-            <input type="email" name="email" placeholder="Votre adresse mail" />
-            <textarea name="message" placeholder="Votre message" rows={5} />
+            <input
+                className="input"
+                type="email"
+                name="email"
+                placeholder="Votre adresse mail"
+            />
+            <textarea
+                className="textarea"
+                name="message"
+                placeholder="Votre message"
+                rows={5}
+            />
             <Button type="submit" className="button">
                 {isPending ? "En cours d'envoi..." : 'Me Contacter'}
             </Button>
