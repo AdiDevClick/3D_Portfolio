ffe88e1030917dca10d95ae6a8efefec0d3e6c47
Adrien <adrienquijo@gmail.com>
2025-07-09 00:39:10 +0200

    Ajout d'un fichier contenant les types pour le formcontact - TODO : les types pour le form contact

--- src/components/3DComponents/Forms/FormsTypes.ts
+++ src/components/3DComponents/Forms/FormsTypes.ts
@@ -1,1 +1,18 @@
-export type HexCellProps = { scalar: number };
+export type handleClickProps = {
+    e: React.MouseEvent<HTMLInputElement | HTMLTextAreaElement>;
+    isEditing: boolean;
+    focused: boolean;
+    setIsEditing: (isEditing: boolean) => void;
+    setFocused: (focused: boolean) => void;
+    inputRef: React.RefObject<HTMLInputElement | HTMLTextAreaElement>;
+};
+
+export type formState = {
+    isEditing: boolean;
+    setIsEditing: (isEditing: boolean) => void;
+    focused: boolean;
+    setFocused: (focused: boolean) => void;
+    inputRef: React.RefObject<HTMLInputElement | HTMLTextAreaElement>;
+    value: string;
+    setValue: (value: string) => void;
+};
