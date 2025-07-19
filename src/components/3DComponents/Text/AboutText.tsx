96f70463824f01642dc253e992a3f67bac55017b
Adrien <adrienquijo@gmail.com>
2025-07-19 08:42:19 +0200

    Code cleanup

--- src/components/3DComponents/Text/AboutText.tsx
+++ src/components/3DComponents/Text/AboutText.tsx
@@ -1,31 +1,15 @@
 import { Text } from '@react-three/drei';
 import { importedNormalFont } from '@/configs/3DFonts.config';
 
-export function AboutText({
-    children,
-    isMobile,
-    // generalScaleX,
-    // contentWidth,
-    // materials,
-    // index,
-    // position = [0, 0, -0.3],
-    // outlineWidth = isMobile ? 0.002 : 0.002,
-    // textAlign = 'left',
-    // maxWidth = isMobile ? contentWidth - 0.4 : contentWidth / 2,
-    ...props
-}) {
+export function AboutText({ children, isMobile, ...props }) {
     return (
         <Text
-            // position={position}
-            // fontSize={(isMobile ? 0.6 : 0.5) * generalScaleX}
             outlineWidth={isMobile ? 0.005 : 0.004}
             outlineColor="black"
             anchorY="top"
-            // textAlign={textAlign}
             anchorX="center"
             color={'black'}
             textAlign={'left'}
-            // maxWidth={maxWidth}
             font={importedNormalFont}
             fontWeight={700}
             userData={{ isWrappedText: true }}
